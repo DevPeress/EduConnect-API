39ee74f11b1212d2e265e550919ccaf2d4e135ce
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-17 17:10:21 -0300

    Ajustando de MiddleWare para MiddleWares

--- WebApplication1/MiddleWares/CorsConfiguration.cs
+++ WebApplication1/MiddleWares/CorsConfiguration.cs
@@ -1,14 +1,14 @@
-﻿namespace EduConnect.MiddleWare;
+﻿namespace EduConnect.MiddleWares;
 
-public class CorsConfiguration
+public static class CorsConfiguration
 {
-    public static IServiceCollection AddConfigureCors(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddCorsConfiguration(this IServiceCollection services)
     {
         services.AddCors(p =>
         {
             p.AddPolicy("restrictivePolicy", policy =>
             {
-                policy.WithOrigins(configuration["ALLOWED_HOSTS"] || "http://localhost:5173")
+                policy.WithOrigins("http://localhost:5173")
                       .AllowAnyMethod()
                       .AllowAnyHeader()
                       .AllowCredentials();
