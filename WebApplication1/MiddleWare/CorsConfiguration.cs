bb8372dc054e014d874303ad48ac4977a6a3133a
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-17 17:03:35 -0300

    Adicionando Configuração do Cors no Projeto

---
+++ WebApplication1/MiddleWare/CorsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace EduConnect.MiddleWare;
+
+public class CorsConfiguration
+{
+    public static IServiceCollection AddConfigureCors(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(p =>
+        {
+            p.AddPolicy("restrictivePolicy", policy =>
+            {
+                policy.WithOrigins(configuration["ALLOWED_HOSTS"] || "http://localhost:5173")
+                      .AllowAnyMethod()
+                      .AllowAnyHeader()
+                      .AllowCredentials();
+            });
+        });
+
+        return services;
+    }
+}
