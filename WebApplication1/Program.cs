721bd2d1c5b6f7a28eabadfa1c29c3d8145c838d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-17 17:04:04 -0300

    Executando o Cors no projeto

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,10 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
-
+builder.Services.AddConfigureCors(builder.Configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
