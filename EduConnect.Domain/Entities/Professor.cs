2d33aa5de499a01b904c41dd24bbe868c6fa6e6e
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:35:15 -0300

    Criação do Objeto Professor

---
+++ EduConnect.Domain/Entities/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Domain;
+
+public class Professor : Pessoa
+{
+    [Key]
+    public required string Codigo { get; set; }
+    public required List<string> Turmas { get; set; }
+    public required string Disciplina { get; set; }
+    public required DateTime Contratacao { get; set; }
+    public required string Formacao { get; set; }
+    public required string Especializacao { get; set; }
+    public required decimal Salario { get; set; }
+    public required string HorarioAula { get; set; }
+}
