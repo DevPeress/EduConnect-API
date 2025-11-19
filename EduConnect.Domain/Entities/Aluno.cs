dbe14565cd7fccf5608976e20655286578501b36
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:35:03 -0300

    Criação do Objeto Aluno

---
+++ EduConnect.Domain/Entities/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Domain;
+
+public class Aluno : Pessoa
+{
+    [Key]
+    public required string Matricula { get; set; }
+    public required string Turma { get; set; }
+    public required int Media { get; set; }
+    public required DateTime DataMatricula { get; set; }
+}
