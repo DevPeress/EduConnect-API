8bbd416a18af4671bc64979899fdd5feffa39dc9
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:34:03 -0300

    Adição do ApiConfiguration para futuramente configurar dados

---
+++ WebApplication1/MiddleWares/ApiConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EduConnect.MiddleWares;
+
+public static class ApiConfiguration
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+    {
+        return services;
+    }
+}
