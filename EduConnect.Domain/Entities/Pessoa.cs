f81fff4240bfffbcd568a48638c1a2f5eaef699e
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:34:55 -0300

    Criação do Objeto Pessoa para utilizar como base de outros

---
+++ EduConnect.Domain/Entities/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace EduConnect.Domain;
+
+public class Pessoa
+{
+    public required string Nome { get; set; }
+    public required string Email { get; set; }
+    public required string Telefone { get; set; }
+    public required string Status { get; set; }
+    public required DateTime Nasc { get; set; }
+    public required string Endereco { get; set; }
+    public required string Cpf { get; set; }
+    public required string ContatoEmergencia { get; set; }
+}
