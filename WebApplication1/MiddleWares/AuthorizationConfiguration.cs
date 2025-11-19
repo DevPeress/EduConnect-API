9ff3b783eb16871e482f2463ad116449bac75f8d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:34:35 -0300

    Adição do AuthorizationConfiguration para futuramente configurar autenticação

---
+++ WebApplication1/MiddleWares/AuthorizationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace EduConnect.MiddleWares;
+
+public static class AuthorizationConfiguration
+{
+    public static IServiceCollection AddAuthorizationConfiguration(this IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminPolicy", policy =>
+                policy.RequireRole("Admin"));
+            options.AddPolicy("UserPolicy", policy =>
+                policy.RequireRole("User", "Admin"));
+        });
+        return services;
+    }
+}
