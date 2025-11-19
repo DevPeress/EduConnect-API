3d8c74970ca216c01f3d7cc7007e504e49633375
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:35:09 -0300

    Criação do Objeto Funcionário

---
+++ EduConnect.Domain/Entities/Funcionario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Domain;
+
+public class Funcionario : Pessoa
+{
+    [Key]
+    public required string Codigo { get; set; }
+    public required string Cargo { get; set; }
+    public required DateTime DataAdmissao { get; set; }
+    public required decimal Salario { get; set; }
+    public required string Departamento { get; set; }
+    public required string Supervisor { get; set; }
+    public required string Turno { get; set; }
+}
