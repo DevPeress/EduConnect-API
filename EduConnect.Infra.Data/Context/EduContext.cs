5589ec160a32f006903f709a35a01bbc3cae830e
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-19 16:36:09 -0300

    Adição do EduContext para criar tabelas, conexão com o banco de dados

---
+++ EduConnect.Infra.Data/Context/EduContext.cs
@@ -0,0 +1,11 @@
+﻿using EduConnect.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduConnect.Infra.Data.Context;
+
+public class EduContext (DbContextOptions<EduContext> options) : DbContext(options)
+{
+    public virtual DbSet<Aluno> Alunos { get; set; }
+    public virtual DbSet<Professor> Professores { get; set; }
+    public virtual DbSet<Funcionario> Funcionarios { get; set; }
+    }
