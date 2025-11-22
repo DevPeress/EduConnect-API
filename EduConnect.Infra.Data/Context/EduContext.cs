b75a285c06f908e1f094b0acefc0c2b2658fb98d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-22 19:16:41 -0300

    Remoção do Virtual dos DbSet

--- EduConnect.Infra.Data/Context/EduContext.cs
+++ EduConnect.Infra.Data/Context/EduContext.cs
@@ -5,8 +5,8 @@ namespace EduConnect.Infra.Data.Context;
 
 public class EduContext(DbContextOptions<EduContext> options) : DbContext(options)
 {
-    public virtual DbSet<Aluno> Alunos { get; set; }
-    public virtual DbSet<Professor> Professores { get; set; }
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<Professor> Professores { get; set; }
     public virtual DbSet<Funcionario> Funcionarios { get; set; }
     private readonly string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EduConnect;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
 
