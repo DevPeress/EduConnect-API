1bdac23e47b9fbb80f8e9a628eba23d8a9dde936
Peres <fabricioperesdsantos@gmail.com>
2025-11-22 23:40:41 -0300

    Removendo using desnecessários

--- EduConnect.Domain/Interfaces/IAlunoRepository.cs
+++ EduConnect.Domain/Interfaces/IAlunoRepository.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EduConnect.Domain.Interfaces;
+﻿namespace EduConnect.Domain.Interfaces;
 
 public interface IAlunoRepository
 {
