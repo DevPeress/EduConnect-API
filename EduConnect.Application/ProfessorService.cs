b24d7abf0da5cd8e4c10ed267c0a4b3fcbdd15f7
Peres <fabricioperesdsantos@gmail.com>
2025-11-22 23:57:14 -0300

    Criando o ProfessorService

---
+++ EduConnect.Application/ProfessorService.cs
@@ -0,0 +1,29 @@
+﻿using EduConnect.Domain;
+using EduConnect.Domain.Interfaces;
+
+namespace EduConnect.Application;
+
+public class ProfessorService(IProfessorRepository repo)
+{
+    private readonly IProfessorRepository _professorRepository = repo;
+    public async Task<List<Professor>> GetAllProfessorAsync()
+    {
+        return await _professorRepository.GetAllAsync();
+    }
+    public async Task<Professor?> GetProfessorByIdAsync(string matricula)
+    {
+        return await _professorRepository.GetByIdAsync(matricula);
+    }
+    public async Task AddProfessorAsync(Professor professor)
+    {
+        await _professorRepository.AddAsync(professor);
+    }
+    public async Task UpdateProfessorAsync(Professor professor)
+    {
+        await _professorRepository.UpdateAsync(professor);
+    }
+    public async Task DeleteProfessorAsync(string matricula)
+    {
+        await _professorRepository.DeleteAsync(matricula);
+    }
+}
