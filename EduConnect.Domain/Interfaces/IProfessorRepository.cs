1a5ade30f0ef485f93cc221c9173ae16ec210948
Peres <fabricioperesdsantos@gmail.com>
2025-11-22 23:44:04 -0300

    Criando IRepository de Professor

---
+++ EduConnect.Domain/Interfaces/IProfessorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EduConnect.Domain.Interfaces;
+
+public interface IProfessorRepository
+{
+    Task<List<Professor>> GetAllAsync();
+    Task<Professor?> GetByIdAsync(string matricula);
+    Task AddAsync(Professor aluno);
+    Task UpdateAsync(Professor aluno);
+    Task DeleteAsync(string matricula);
+
+}
