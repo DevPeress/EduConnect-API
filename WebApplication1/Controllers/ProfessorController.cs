914cdda8044bbd81f08954f72ae2a988f1a9d336
Peres <fabricioperesdsantos@gmail.com>
2025-11-23 00:11:44 -0300

    Criando ProfessorController para API

---
+++ WebApplication1/Controllers/ProfessorController.cs
@@ -0,0 +1,62 @@
+﻿using EduConnect.Application;
+using EduConnect.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduConnect.Controllers
+{
+    [ApiController]
+    [Route("api/professores")]
+    public class ProfessorController(ProfessorService service) : ControllerBase
+    {
+        private readonly ProfessorService _professorService = service;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProfessor()
+        {
+            var professores = await _professorService.GetAllProfessorAsync();
+            return Ok(professores);
+        }
+        [HttpGet("{matricula}")]
+        public async Task<IActionResult> GetProfessorById(string matricula)
+        {
+            var professores = await _professorService.GetProfessorByIdAsync(matricula);
+            if (professores == null)
+            {
+                return NotFound();
+            }
+            return Ok(professores);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddProfessor(Professor professor)
+        {
+            await _professorService.AddProfessorAsync(professor);
+            return CreatedAtAction(nameof(GetProfessorById), new { matricula = professor.Registro }, professor);
+        }
+        [HttpPut("{matricula}")]
+        public async Task<IActionResult> UpdateProfessor(string matricula, Professor professor)
+        {
+            if (matricula != professor.Registro)
+            {
+                return BadRequest();
+            }
+            var existingProfessor = await _professorService.GetProfessorByIdAsync(matricula);
+            if (existingProfessor == null)
+            {
+                return NotFound();
+            }
+            await _professorService.UpdateProfessorAsync(professor);
+            return NoContent();
+        }
+        [HttpDelete("{matricula}")]
+        public async Task<IActionResult> DeleteAluno(string matricula)
+        {
+            var existingAluno = await _professorService.GetProfessorByIdAsync(matricula);
+            if (existingAluno == null)
+            {
+                return NotFound();
+            }
+            await _professorService.DeleteProfessorAsync(matricula);
+            return NoContent();
+        }
+    }
+}
