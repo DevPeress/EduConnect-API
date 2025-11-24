b341391da4b9cd74b3c7ff4ee97388e8c98fc281
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 15:00:18 -0300

    Migration para ajustar o Salario e Dados do professore

---
+++ EduConnect.Infra.Data/Migrations/20251124175459_Professores.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduConnect.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Professores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Especializacao",
+                table: "Professores");
+
+            migrationBuilder.DropColumn(
+                name: "HorarioAula",
+                table: "Professores");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Especializacao",
+                table: "Professores",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HorarioAula",
+                table: "Professores",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
