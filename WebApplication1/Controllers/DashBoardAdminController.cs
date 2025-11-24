cee324a33a35d0de7090214b402b4c3d14617746
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 14:08:44 -0300

    Criação da API DashBoardAdmin

---
+++ WebApplication1/Controllers/DashBoardAdminController.cs
@@ -0,0 +1,37 @@
+﻿using EduConnect.Application.Services;
+using EduConnect.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduConnect.Controllers
+{
+    [ApiController]
+    [Route("api/dashboardadmin")]
+    public class DashBoardAdminController(DashBoardAdminService service) : ControllerBase
+    {
+        private readonly DashBoardAdminService _dashBoardAdminService = service;
+        [HttpGet("Cards")]
+        public async Task<IActionResult> GetCardsData()
+        {
+            var totalAlunos = await _dashBoardAdminService.GetTotalAlunosAsync();
+            var totalProfessores = await _dashBoardAdminService.GetTotalProfessoresAsync();
+            var (aumentoAlunos, aumentoProfessores) = await _dashBoardAdminService.GetAumentoAsync();
+            var (porcentagemAlunos, porcentagemProfessores) = await _dashBoardAdminService.GetPorcentagemAsync();
+            return Ok(new List<CardsAdminDto>
+            {
+                new CardsAdminDto                 {
+                    Dado = "Alunos",
+                    Total = totalAlunos,
+                    Aumento = aumentoAlunos,
+                    Porcentagem = porcentagemAlunos
+                },
+                new CardsAdminDto
+                {
+                    Dado = "Professores",
+                    Total = totalProfessores,
+                    Aumento = aumentoProfessores,
+                    Porcentagem = porcentagemProfessores
+                }
+            });
+        }
+    }
+}
