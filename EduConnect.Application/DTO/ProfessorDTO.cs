066235a67d20de780efc4006203a00fd01330059
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 13:12:20 -0300

    Troca do DateTime para DateOnly

--- EduConnect.Application/DTO/ProfessorDTO.cs
+++ EduConnect.Application/DTO/ProfessorDTO.cs
@@ -6,14 +6,14 @@ public class ProfessorDTO
     public string Email { get; set; } = null!;
     public string Telefone { get; set; } = null!;
     public string Status { get; set; } = null!;
-    public DateTime Nasc { get; set; }
+    public DateOnly Nasc { get; set; }
     public string Endereco { get; set; } = null!;
     public string Cpf { get; set; } = null!;
     public string ContatoEmergencia { get; set; } = null!;
     public string Registro { get; set; } = null!;
     public List<string> Turmas { get; set; } = null!;
     public string Disciplina { get; set; } = null!;
-    public DateTime Contratacao { get; set; }
+    public DateOnly Contratacao { get; set; }
     public string Formacao { get; set; } = null!;
     public string Especializacao { get; set; } = null!;
     public decimal Salario { get; set; }
