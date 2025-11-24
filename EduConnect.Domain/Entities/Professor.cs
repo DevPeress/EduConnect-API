d599f59ef7b2c766ab8ae1a1bdc8492b0fac695b
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 13:12:09 -0300

    Troca do DateTime para DateOnly

--- EduConnect.Domain/Entities/Professor.cs
+++ EduConnect.Domain/Entities/Professor.cs
@@ -4,7 +4,7 @@ public class Professor : Pessoa
 {
     public required List<string> Turmas { get; set; }
     public required string Disciplina { get; set; }
-    public required DateTime Contratacao { get; set; }
+    public required DateOnly Contratacao { get; set; }
     public required string Formacao { get; set; }
     public required string Especializacao { get; set; }
     public required decimal Salario { get; set; }
