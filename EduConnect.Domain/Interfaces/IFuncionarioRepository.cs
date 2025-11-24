8a068f8c9ece4f95ed4186c2e79964aa08136567
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 09:58:17 -0300

    Criação da Interface dos Funcionarios

---
+++ EduConnect.Domain/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace EduConnect.Domain.Interfaces;
+
+public interface IFuncionarioRepository
+{
+    Task<List<Funcionario>> GetAllAsync();
+    Task<Funcionario?> GetByIdAsync(string matricula);
+    Task AddAsync(Funcionario funcionario);
+    Task UpdateAsync(Funcionario funcionario);
+    Task DeleteAsync(string matricula);
+}
