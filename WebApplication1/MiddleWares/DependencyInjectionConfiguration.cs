bfe1711758ddfed8ccee01e069677bcf7a6cfe6f
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 16:35:35 -0300

    Criando Middleware de Injection para levar ao Injection do Infra

---
+++ WebApplication1/MiddleWares/DependencyInjectionConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using EduConnect.Infra.IoC;
+
+namespace EduConnect.MiddleWares
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddDependencyInjectionConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            Injection.RegisterServices(services, configuration);
+        }
+    }
+}
