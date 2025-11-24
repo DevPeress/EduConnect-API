6f70e44bf086b209fb802d9d57ed8f7a43067ca7
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 13:12:03 -0300

    Troca do DateTime para DateOnly

--- EduConnect.Domain/Entities/Funcionario.cs
+++ EduConnect.Domain/Entities/Funcionario.cs
@@ -3,7 +3,7 @@
 public class Funcionario : Pessoa
 {
     public required string Cargo { get; set; }
-    public required DateTime DataAdmissao { get; set; }
+    public required DateOnly DataAdmissao { get; set; }
     public required decimal Salario { get; set; }
     public required string Departamento { get; set; }
     public required string Supervisor { get; set; }
