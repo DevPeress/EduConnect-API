0b97d82dfc2e6cbd169f498d4821e73cc8565dfd
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 13:12:17 -0300

    Troca do DateTime para DateOnly

--- EduConnect.Application/DTO/FuncionarioDTO.cs
+++ EduConnect.Application/DTO/FuncionarioDTO.cs
@@ -6,13 +6,13 @@ public class FuncionarioDTO
     public string Email { get; set; } = null!;
     public string Telefone { get; set; } = null!;
     public string Status { get; set; } = null!;
-    public DateTime Nasc { get; set; }
+    public DateOnly Nasc { get; set; }
     public string Endereco { get; set; } = null!;
     public string Cpf { get; set; } = null!;
     public string ContatoEmergencia { get; set; } = null!;
     public string Registro { get; set; } = null!;
     public string Cargo { get; set; } = null!;
-    public DateTime DataAdmissao { get; set; }
+    public DateOnly DataAdmissao { get; set; }
     public decimal Salario { get; set; }
     public string Departamento { get; set; } = null!;
     public string Supervisor { get; set; } = null!;
