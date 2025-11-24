8fd8021959a40a6662c48ac407bcbf341d6a9405
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 15:00:57 -0300

    Ajustando Salario para vir com valor padrão

--- EduConnect.Domain/Entities/Funcionario.cs
+++ EduConnect.Domain/Entities/Funcionario.cs
@@ -4,7 +4,7 @@ public class Funcionario : Pessoa
 {
     public required string Cargo { get; set; }
     public required DateOnly DataAdmissao { get; set; }
-    public required decimal Salario { get; set; }
+    public required decimal Salario { get; set; } = decimal.Zero;
     public required string Departamento { get; set; }
     public required string Supervisor { get; set; }
     public required string Turno { get; set; }
