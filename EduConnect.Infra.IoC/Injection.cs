397a76f76fc66131be50d12ed053166f894fd0c8
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 16:35:17 -0300

    Criação do Infra.IoC e arquivo Injection para adicionar os Scoped

---
+++ EduConnect.Infra.IoC/Injection.cs
@@ -0,0 +1,27 @@
+﻿using EduConnect.Application.Services;
+using EduConnect.Domain.Interfaces;
+using EduConnect.Infra.Data.Context;
+using EduConnect.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EduConnect.Infra.IoC
+{
+    public class Injection
+    {
+        public static void RegisterServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ProfessorRepository, ProfessorRepository>();
+            services.AddDbContext<EduContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAlunoRepository, AlunoRepository>();
+            services.AddScoped<AlunoService>();
+            services.AddScoped<IProfessorRepository, ProfessorRepository>();
+            services.AddScoped<ProfessorService>();
+            services.AddScoped<IDashboardAdminRepository, DashBoardAdminRepository>();
+            services.AddScoped<DashBoardAdminService>();
+        }
+    }
+}
