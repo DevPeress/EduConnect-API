5e6c8216ccf3b8e1fbbe094cd70c5907df1abd4d
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 09:59:00 -0300

    Criaçaõ do Service do Funcionario

---
+++ EduConnect.Application/Services/FuncionarioService.cs
@@ -0,0 +1,29 @@
+﻿using EduConnect.Domain;
+using EduConnect.Domain.Interfaces;
+
+namespace EduConnect.Application.Services;
+
+public class FuncionarioService(IFuncionarioRepository repo)
+{
+    private readonly IFuncionarioRepository _funcionarioRepository = repo;
+    public async Task<List<Funcionario>> GetAllFuncionariosAsync()
+    {
+        return await _funcionarioRepository.GetAllAsync();
+    }
+    public async Task<Funcionario?> GetFuncionarioByIdAsync(string matricula)
+    {
+        return await _funcionarioRepository.GetByIdAsync(matricula);
+    }
+    public async Task AddFuncionarioAsync(Funcionario funcionario)
+    {
+        await _funcionarioRepository.AddAsync(funcionario);
+    }
+    public async Task UpdateFuncionarioAsync(Funcionario funcionario)
+    {
+        await _funcionarioRepository.UpdateAsync(funcionario);
+    }
+    public async Task DeleteFuncionarioAsync(string matricula)
+    {
+        await _funcionarioRepository.DeleteAsync(matricula);
+    }
+}
