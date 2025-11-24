fc877f14f1b01bad3916621b20d13d86579b7ad7
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 14:08:18 -0300

    Criação da Interface do Repositorio do DashBoardAdmin

---
+++ EduConnect.Domain/Interfaces/IDashBoardAdminRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EduConnect.Domain.Interfaces;
+
+public interface IDashboardAdminRepository
+{
+    Task<List<Aluno>> GetAllAlunosAsync();
+    Task<List<Professor>> GetAllProfessoresAsync();
+    Task<int> GetAumentoAlunos();
+    Task<int> GetAumentProfessores();
+    Task<double> GetPorcentagemAlunos();
+    Task<double> GetPorcentagemProfessores();
+}
