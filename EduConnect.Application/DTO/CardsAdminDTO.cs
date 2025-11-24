19429a3e17b5c81a086e8db0c0134258742b5c73
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 14:07:48 -0300

    Crição do DTO Cards Admin para definir dados que serão enviados

---
+++ EduConnect.Application/DTO/CardsAdminDTO.cs
@@ -0,0 +1,7 @@
+﻿public class CardsAdminDto
+{
+    public string Dado { get; set; }
+    public double Total { get; set; }
+    public double Aumento { get; set; }
+    public double Porcentagem { get; set; }
+}
