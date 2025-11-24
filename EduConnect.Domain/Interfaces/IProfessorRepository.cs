aea15c4e954eb0c8c49e8506f2effcd572fd4714
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-24 15:00:39 -0300

    Adicionando a pesquisa do último professor cadastro

--- EduConnect.Domain/Interfaces/IProfessorRepository.cs
+++ EduConnect.Domain/Interfaces/IProfessorRepository.cs
@@ -4,6 +4,7 @@ public interface IProfessorRepository
 {
     Task<List<Professor>> GetAllAsync();
     Task<Professor?> GetByIdAsync(string matricula);
+    Task<Professor?> GetLastProfessorAsync();
     Task AddAsync(Professor professor);
     Task UpdateAsync(Professor professor);
     Task DeleteAsync(string matricula);
