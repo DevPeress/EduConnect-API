8755af2a89a5417bbc249b51e4bf9a17a8aecedb
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 14:41:19 -0300

    Criando a entidade do Registro

---
+++ EduConnect.Domain/Entities/Registro.cs
@@ -0,0 +1,9 @@
+﻿namespace EduConnect.Domain.Entities;
+
+public class Registro
+{
+    public required Guid Id { get; set; }
+    public required string Tipo { get; set; }
+    public required string Descricao { get; set; }
+    public required DateTime Horario { get; set; }
+}
