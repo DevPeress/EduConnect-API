fc94a4dd1f59493440377fd1ae13cfcd5bf1df90
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 17:43:18 -0300

    Adicionando Guid Id ao FuncionarioDTO

--- EduConnect.Application/DTO/FuncionarioDTO.cs
+++ EduConnect.Application/DTO/FuncionarioDTO.cs
@@ -2,6 +2,7 @@
 
 public class FuncionarioDTO
 {
+    public Guid Id { get; set; }
     public string Nome { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Telefone { get; set; } = null!;
