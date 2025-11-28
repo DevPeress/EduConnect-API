5146dc615794258ccf1dcafd0f4d862fb3e71ff3
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 14:41:06 -0300

    Criando o Service para Registros

---
+++ EduConnect.Application/Services/RegistroService.cs
@@ -0,0 +1,48 @@
+﻿using EduConnect.Application.DTO;
+using EduConnect.Domain.Entities;
+using EduConnect.Domain.Interfaces;
+
+namespace EduConnect.Application.Services;
+
+public class RegistroService(IRegistroRepository repo)
+{
+    private readonly IRegistroRepository _registroRepository = repo;
+    public async Task<List<Registro>> GetAllRegistrosAsync()
+    {
+        return await _registroRepository.GetRegistrosAsync();
+    }
+    public async Task<List<Registro>> GetLastRegistrosAsync()
+    {
+        return await _registroRepository.GetLastRegistrosAync();
+    }
+    public async Task<Registro?> GetRegistroByIdAsync(Guid id)
+    {
+        return await _registroRepository.GetRegistroByIdAsync(id);
+    }
+    public async Task AddRegistroAsync(RegistroDTO dto)
+    {
+        var registro = new Registro
+        {
+            Id = Guid.NewGuid(),
+            Tipo = dto.Tipo,
+            Descricao = dto.Descricao,
+            Horario = DateTime.Now
+        };
+        await _registroRepository.AddRegistroAsync(registro);
+    }
+    public async Task UpdateRegistroAsync(RegistroDTO dto)
+    {
+        var registro = new Registro
+        {
+            Id = dto.Id,
+            Tipo = dto.Tipo,
+            Descricao = dto.Descricao,
+            Horario = dto.Horario
+        };
+        await _registroRepository.UpdateRegistroAsync(registro);
+    }
+    public async Task DeleteRegistroAsync(Guid id)
+    {
+        await _registroRepository.DeleteRegistroAsync(id);
+    }
+}
