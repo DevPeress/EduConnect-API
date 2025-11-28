d3bacb687289f61e7ffc57fc447fa29185bce93e
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 14:40:58 -0300

    Criando o DTO para Registros

---
+++ EduConnect.Application/DTO/RegistroDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EduConnect.Application.DTO;
+public class RegistroDTO
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Tipo { get; set; } = null!;
+    public string Descricao { get; set; } = null!;
+    public DateTime Horario { get; set; } = DateTime.Now;
+}
