c3b47359bd280ae84c5b01cd91fa8ccee4b59fcc
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 14:41:34 -0300

    Criando Interface do Repositorio dos Registros

---
+++ EduConnect.Domain/Interfaces/IRegistroRepository.cs
@@ -0,0 +1,13 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Domain.Interfaces;
+
+public interface IRegistroRepository
+{
+    Task<List<Registro>> GetRegistrosAsync();
+    Task<List<Registro>> GetLastRegistrosAync();
+    Task<Registro?> GetRegistroByIdAsync(Guid id);
+    Task AddRegistroAsync(Registro registro);
+    Task UpdateRegistroAsync(Registro registro);
+    Task DeleteRegistroAsync(Guid id);
+}
