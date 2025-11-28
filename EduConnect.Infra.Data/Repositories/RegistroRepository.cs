2c42b144882f455f5ffead0d3bd1ef2463a21756
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 14:41:42 -0300

    Criando o Repositorio do Registro

---
+++ EduConnect.Infra.Data/Repositories/RegistroRepository.cs
@@ -0,0 +1,44 @@
+﻿using EduConnect.Domain.Entities;
+using EduConnect.Domain.Interfaces;
+using EduConnect.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduConnect.Infra.Data.Repositories;
+public class RegistroRepository(EduContext context) : IRegistroRepository
+{
+    private readonly EduContext _context = context;
+    public async Task<List<Registro>> GetRegistrosAsync()
+    {
+        return await _context.Registros.ToListAsync();
+    }
+    public async Task<List<Registro>> GetLastRegistrosAync()
+    {
+        return await _context.Registros
+            .OrderBy(r => r.Horario)
+            .Take(10)
+            .ToListAsync();
+    }
+    public async Task<Registro?> GetRegistroByIdAsync(Guid id)
+    {
+        return await _context.Registros.FirstOrDefaultAsync(r => r.Id == id);
+    }
+    public async Task AddRegistroAsync(Registro registro)
+    {
+        await _context.Registros.AddAsync(registro);
+        await _context.SaveChangesAsync();
+    }
+    public async Task UpdateRegistroAsync(Registro registro)
+    {
+        _context.Registros.Update(registro);
+        await _context.SaveChangesAsync();
+    }
+    public async Task DeleteRegistroAsync(Guid id)
+    {
+        var registro = await GetRegistroByIdAsync(id);
+        if (registro != null)
+        {
+            _context.Registros.Remove(registro);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
