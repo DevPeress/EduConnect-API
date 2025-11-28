3fabec008f087f9c8f662c8ff6c10af13de0cd99
Fabrício <fabricioperesdsantos@gmail.com>
2025-11-28 17:45:56 -0300

    Trocando Funcionario para FuncionarioDTO

--- EduConnect.Application/Services/FuncionarioService.cs
+++ EduConnect.Application/Services/FuncionarioService.cs
@@ -1,4 +1,5 @@
-﻿using EduConnect.Domain.Entities;
+﻿using EduConnect.Application.DTO;
+using EduConnect.Domain.Entities;
 using EduConnect.Domain.Interfaces;
 
 namespace EduConnect.Application.Services;
@@ -14,12 +15,50 @@ public class FuncionarioService(IFuncionarioRepository repo)
     {
         return await _funcionarioRepository.GetByIdAsync(id);
     }
-    public async Task AddFuncionarioAsync(Funcionario funcionario)
+    public async Task AddFuncionarioAsync(FuncionarioDTO dto)
     {
+        var funcionario = new Funcionario
+        {
+            Id = Guid.NewGuid(),
+            Nome = dto.Nome,
+            Email = dto.Email,
+            Telefone = dto.Telefone,
+            Status = dto.Status,
+            Nasc = dto.Nasc,
+            Endereco = dto.Endereco,
+            Cpf = dto.Cpf,
+            ContatoEmergencia = dto.ContatoEmergencia,
+            Registro = dto.Registro,
+            Cargo = dto.Cargo,
+            DataAdmissao = dto.DataAdmissao,
+            Salario = dto.Salario,
+            Departamento = dto.Departamento,
+            Supervisor = dto.Supervisor,
+            Turno = dto.Turno
+        };
         await _funcionarioRepository.AddAsync(funcionario);
     }
-    public async Task UpdateFuncionarioAsync(Funcionario funcionario)
+    public async Task UpdateFuncionarioAsync(FuncionarioDTO dto)
     {
+        var funcionario = new Funcionario
+        {
+            Id = dto.Id,
+            Nome = dto.Nome,
+            Email = dto.Email,
+            Telefone = dto.Telefone,
+            Status = dto.Status,
+            Nasc = dto.Nasc,
+            Endereco = dto.Endereco,
+            Cpf = dto.Cpf,
+            ContatoEmergencia = dto.ContatoEmergencia,
+            Registro = dto.Registro,
+            Cargo = dto.Cargo,
+            DataAdmissao = dto.DataAdmissao,
+            Salario = dto.Salario,
+            Departamento = dto.Departamento,
+            Supervisor = dto.Supervisor,
+            Turno = dto.Turno
+        };
         await _funcionarioRepository.UpdateAsync(funcionario);
     }
     public async Task DeleteFuncionarioAsync(Guid id)
