445a92ffe47f8a3a701be8b45ef2c14c5228ff00
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 14:04:34 -0300

    Criando a Interface do Repositório do Financeiro

---
+++ EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
@@ -0,0 +1,16 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Domain.Interfaces;
+
+public interface IFinanceiroRepository
+{
+    Task<List<Financeiro>> GetAll();
+    Task <List<Financeiro>> GetByAlunoId(Guid alunoId);
+    Task <List<Financeiro>> GetByCategoria(string categoria);
+    Task <List<Financeiro>> GetByStatus(string status);
+    Task <List<Financeiro>> GetByDateRange(DateOnly startDate, DateOnly endDate);
+    Task <Financeiro?> GetById(Guid id);
+    Task Add(Financeiro financeiro);
+    Task Update(Financeiro financeiro);
+    Task Delete(Guid id);
+}
