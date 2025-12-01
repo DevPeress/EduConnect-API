80dc367a3fc59d58830c70f184c7bd79acea251b
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 15:22:20 -0300

    Ajustando Financeiro DTO para retornar os dados esperado pelo front

--- EduConnect.Application/DTO/FinanceiroDTO.cs
+++ EduConnect.Application/DTO/FinanceiroDTO.cs
@@ -2,12 +2,14 @@
 
 public class FinanceiroDTO
 {
-    public Guid Id { get; set; }
+    public Guid Registro { get; set; }
     public Guid AlunoId { get; set; }
     public required string Categoria { get; set; }
     public decimal Valor { get; set; }
     public DateOnly DataVencimento { get; set; }
     public bool Pago { get; set; }
     public DateOnly? DataPagamento { get; set; }
+    public string? Aluno { get; set; }
+    public DateOnly? Nasc { get; set; }
     public bool Cancelado { get; set; }
 }
