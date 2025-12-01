3937a57aed5269bf729317cc85627743dce09b37
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 14:04:25 -0300

    Criando o Objeto do Financeiro

---
+++ EduConnect.Domain/Entities/Financeiro.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Domain.Entities;
+
+public class Financeiro
+{
+    [Key]
+    public Guid Id { get; set; }
+    public Guid AlunoId { get; set; }
+    public required string Categoria { get; set; }
+    public decimal Valor { get; set; }
+    public DateOnly DataVencimento { get; set; }
+    public bool Pago { get; set; }
+    public bool Cancelado { get; set; }
+    public DateOnly? DataPagamento { get; set; }
+    public string? Observacoes { get; set; }
+}
