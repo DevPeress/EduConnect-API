e5478646c7919adee2b5303e7f7921c9ac489d5a
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 14:04:10 -0300

    Criando o DTO do Financeiro

---
+++ EduConnect.Application/DTO/FinanceiroDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace EduConnect.Application.DTO;
+
+public class FinanceiroDTO
+{
+    public Guid Id { get; set; }
+    public Guid AlunoId { get; set; }
+    public required string Categoria { get; set; }
+    public decimal Valor { get; set; }
+    public DateOnly DataVencimento { get; set; }
+    public bool Pago { get; set; }
+    public DateOnly? DataPagamento { get; set; }
+    public bool Cancelado { get; set; }
+}
