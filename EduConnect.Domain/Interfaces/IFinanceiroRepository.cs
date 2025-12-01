8e65ceb42a74de9df1ea2765d385b79704a9a942
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-01 16:21:18 -0300

    Ajustando Interface do Financeiro para uma pesquisa geral de filtro

--- EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
+++ EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
@@ -6,9 +6,7 @@ public interface IFinanceiroRepository
 {
     Task<List<Financeiro>> GetAll();
     Task <List<Financeiro>> GetByAlunoId(Guid alunoId);
-    Task <List<Financeiro>> GetByCategoria(string categoria);
-    Task <List<Financeiro>> GetByStatus(string status);
-    Task <List<Financeiro>> GetByDateRange(DateOnly startDate, DateOnly endDate);
+    Task <List<Financeiro>> GetByFilters(string categoria, string status, string data);
     Task <Financeiro?> GetById(Guid id);
     Task Add(Financeiro financeiro);
     Task Update(Financeiro financeiro);
