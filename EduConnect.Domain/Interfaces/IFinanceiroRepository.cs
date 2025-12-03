ce0324c8734126c71386d20d6ba3dcf5cd2346e3
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 12:51:49 -0300

    Alterando para FinanceiroFiltro os dados recebidos do filtro

--- EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
+++ EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
@@ -6,7 +6,7 @@ public interface IFinanceiroRepository
 {
     Task<List<Financeiro>> GetAll();
     Task <List<Financeiro>> GetByAlunoId(Guid alunoId);
-    Task <(IEnumerable<Financeiro>, int TotalRegistro)> GetByFilters(FinanceiroFiltro filtro);
+    Task <List<Financeiro>> GetByFilters(string categoria, string status, string data);
     Task <Financeiro?> GetById(Guid id);
     Task Add(Financeiro financeiro);
     Task Update(Financeiro financeiro);
