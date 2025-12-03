c75a5cfa4c4c2f015b46a9ef37b14612254fcb76
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:44:38 -0300

    Criação do DTO de Turma

---
+++ EduConnect.Application/DTO/TurmaDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace EduConnect.Application.DTO;
+public record TurmaDTO
+{
+    public Guid Registro { get; set; }
+    public required string Nome { get; set; }
+    public required string Turno { get; set; }
+    public Guid ProfessorID { get; set; }
+    public List<Guid> Alunos { get; set; } = [];
+    public Guid SalaID { get; set; }
+    public Guid DisciplinaID { get; set; }
+    public required string Horario { get; set; }
+    public int Capacidade { get; set; }
+    public DateOnly AnoLetivo { get; set; }
+}
