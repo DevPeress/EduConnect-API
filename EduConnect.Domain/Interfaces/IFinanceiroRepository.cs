ef761048c4f05242b2447bcd627367bea9790d4d
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 12:57:44 -0300

    Revert "Alterando para FinanceiroFiltro os dados recebidos do filtro"
    This reverts commit ce0324c8734126c71386d20d6ba3dcf5cd2346e3.

--- EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
+++ EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
@@ -6,7 +6,7 @@ public interface IFinanceiroRepository
 {
     Task<List<Financeiro>> GetAll();
     Task <List<Financeiro>> GetByAlunoId(Guid alunoId);
-    Task <List<Financeiro>> GetByFilters(string categoria, string status, string data);
+    Task <(IEnumerable<Financeiro>, int TotalRegistro)> GetByFilters(FinanceiroFiltro filtro);
     Task <Financeiro?> GetById(Guid id);
     Task Add(Financeiro financeiro);
     Task Update(Financeiro financeiro);
