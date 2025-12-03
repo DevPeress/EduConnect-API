0c2dd1287a29c043d50bfce6d33319a10fbe05a2
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:45:27 -0300

    Criação do repositorio das turmas

---
+++ EduConnect.Infra.Data/Repositories/TurmaRepository.cs
@@ -0,0 +1,37 @@
+﻿using EduConnect.Domain.Entities;
+using EduConnect.Domain.Interfaces;
+using EduConnect.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduConnect.Infra.Data.Repositories;
+public class TurmaRepository(EduContext context) : ITurmaRepository
+{
+    private readonly EduContext _context = context;
+    public async Task<List<Turma>> GetTurmasAsync()
+    {
+        return await _context.Turmas.ToListAsync();
+    }
+    public async Task<Turma?> GetTurmaByIdAsync(Guid id)
+    {
+        return await _context.Turmas.FindAsync(id);
+    }
+    public async Task AddTurmaAsync(Turma turma)
+    {
+        await _context.Turmas.AddAsync(turma);
+        await _context.SaveChangesAsync();
+    }
+    public async Task UpdateTurmaAsync(Turma turma)
+    {
+        _context.Turmas.Update(turma);
+        await _context.SaveChangesAsync();
+    }
+    public async Task DeleteTurmaAsync(Guid id)
+    {
+        var turma = await _context.Turmas.FindAsync(id);
+        if (turma != null)
+        {
+            _context.Turmas.Remove(turma);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
