d69a990ebd0d90198ea6358fb9bedacb0c8fe343
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:48:49 -0300

    Alterando DTO para record e adicionando construtor

--- EduConnect.Application/DTO/CardsAdminDTO.cs
+++ EduConnect.Application/DTO/CardsAdminDTO.cs
@@ -1,7 +1,16 @@
-﻿public class CardsAdminDto
+﻿namespace EduConnect.Application.DTO;
+public record CardsAdminDto
 {
-    public string Dado { get; set; }
-    public double Total { get; set; }
-    public double Aumento { get; set; }
-    public double Porcentagem { get; set; }
+    public required string Dado { get; init; }
+    public double Total { get; init; }
+    public double Aumento { get; init; }
+    public double Porcentagem { get; init; }
+
+    public CardsAdminDto(string dado, double total, double aumento, double porcentagem)
+    {
+        Dado = dado;
+        Total = total;
+        Aumento = aumento;
+        Porcentagem = porcentagem;
+    }
 }
