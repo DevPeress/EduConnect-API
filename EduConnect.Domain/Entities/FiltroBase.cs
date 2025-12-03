7ad34b0e368495c2f8a264ae5bb1ae0f9d17e463
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 16:16:18 -0300

    Adicionando função para alterar o limite

--- EduConnect.Domain/Entities/FiltroBase.cs
+++ EduConnect.Domain/Entities/FiltroBase.cs
@@ -9,4 +9,9 @@ public class FiltroBase
     {
         return (Page == 1) ? 0 : (Page - 1) * Limit;
     }
+
+    public void AlterarLimit(int novoLimit)
+    {
+        Limit = novoLimit;
+    }
 }
