43fb4afd172134e62b72e4ad7583e18dc8b7d5bf
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 13:31:49 -0300

    Adicionando ID

--- EduConnect.Application/DTO/TurmaDTO.cs
+++ EduConnect.Application/DTO/TurmaDTO.cs
@@ -1,10 +1,11 @@
 ﻿namespace EduConnect.Application.DTO;
 public record TurmaDTO
 {
-    public int Registro { get; set; }
+    public int Id { get; set; }
+    public string Registro { get; set; }
     public required string Nome { get; set; }
     public required string Turno { get; set; }
-    public Guid ProfessorID { get; set; }
+    public int ProfessorID { get; set; }
     public List<int> Alunos { get; set; } = [];
     public int SalaID { get; set; }
     public int DisciplinaID { get; set; }
