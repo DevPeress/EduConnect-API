ceaf8d2b0658b153cfbb039db6e98247b4d0bf4d
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:43:49 -0300

    Criação do TurmaController

---
+++ WebApplication1/Controllers/TurmaController.cs
@@ -0,0 +1,51 @@
+﻿using EduConnect.Application.DTO;
+using EduConnect.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduConnect.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TurmaController(TurmaService TurmaService) : ControllerBase
+    {
+        private readonly TurmaService _turmaService = TurmaService;
+        [HttpGet]
+        public async Task<IActionResult> GetAllTurmas()
+        {
+            var turmas = await _turmaService.GetAllTurmasAsync();
+            return Ok(turmas);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTurmaById(Guid id)
+        {
+            var turma = await _turmaService.GetTurmaByIdAsync(id);
+            if (turma == null)
+            {
+                return NotFound();
+            }
+            return Ok(turma);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTurma(TurmaDTO turmaDTO)
+        {
+            await _turmaService.AddTurmaAsync(turmaDTO);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTurma(Guid id, TurmaDTO turmaDTO)
+        {
+            if (id != turmaDTO.Registro)
+            {
+                return BadRequest();
+            }
+            await _turmaService.UpdateTurmaAsync(turmaDTO);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTurma(Guid id)
+        {
+            await _turmaService.DeleteTurmaAsync(id);
+            return NoContent();
+        }
+    }
+}
