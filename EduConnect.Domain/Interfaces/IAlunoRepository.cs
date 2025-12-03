702ca6a19ffb028c485494a44e154998762229b2
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 13:30:17 -0300

    Alterando guid para int

--- EduConnect.Domain/Interfaces/IAlunoRepository.cs
+++ EduConnect.Domain/Interfaces/IAlunoRepository.cs
@@ -5,10 +5,10 @@ namespace EduConnect.Domain.Interfaces;
 public interface IAlunoRepository
 {
     Task<List<Aluno>> GetAllAsync();
-    Task<Aluno?> GetByIdAsync(Guid id);
+    Task<Aluno?> GetByIdAsync(int id);
     Task<Aluno?> GetLastAlunoAsync();
     Task AddAsync(Aluno aluno);
     Task UpdateAsync(Aluno aluno);
-    Task DeleteAsync(Guid id);
+    Task DeleteAsync(int id);
 
 }
