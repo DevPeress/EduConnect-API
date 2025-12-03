3ddde39595cc736981a45032f4df0f80fc525fde
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 12:56:09 -0300

    Removendo construtor

--- EduConnect.Application/DTO/CardsAdminDTO.cs
+++ EduConnect.Application/DTO/CardsAdminDTO.cs
@@ -5,12 +5,4 @@ public record CardsAdminDto
     public double Total { get; init; }
     public double Aumento { get; init; }
     public double Porcentagem { get; init; }
-
-    public CardsAdminDto(string dado, double total, double aumento, double porcentagem)
-    {
-        Dado = dado;
-        Total = total;
-        Aumento = aumento;
-        Porcentagem = porcentagem;
-    }
 }
