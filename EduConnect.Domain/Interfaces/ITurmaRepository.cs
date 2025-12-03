16cc7f78e4626005f6b7d7937ac732aeac61ca2e
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:45:19 -0300

    Criação da interface de turma

---
+++ EduConnect.Domain/Interfaces/ITurmaRepository.cs
@@ -0,0 +1,12 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Domain.Interfaces;
+
+public interface ITurmaRepository
+{
+    Task<List<Turma>> GetTurmasAsync();
+    Task<Turma?> GetTurmaByIdAsync(Guid id);
+    Task AddTurmaAsync(Turma turma);
+    Task UpdateTurmaAsync(Turma turma);
+    Task DeleteTurmaAsync(Guid id);
+}
