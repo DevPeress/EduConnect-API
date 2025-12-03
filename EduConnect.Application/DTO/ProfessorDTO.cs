4be30e661259480ef1b96a3f9f2e4a9bd60c0b8c
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 12:49:51 -0300

    Alterando DTO para record e adicionando construtor

--- EduConnect.Application/DTO/ProfessorDTO.cs
+++ EduConnect.Application/DTO/ProfessorDTO.cs
@@ -1,20 +1,41 @@
-﻿namespace EduConnect.Application.DTO;
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO;
 
 public record ProfessorDTO
 {
-    public Guid Id { get; set; }
-    public string Nome { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Telefone { get; set; } = null!;
-    public string Status { get; set; } = null!;
-    public DateOnly Nasc { get; set; }
-    public string Endereco { get; set; } = null!;
-    public string Cpf { get; set; } = null!;
-    public string ContatoEmergencia { get; set; } = null!;
-    public string Registro { get; set; } = null!;
-    public List<string> Turmas { get; set; } = null!;
-    public string Disciplina { get; set; } = null!;
-    public DateOnly Contratacao { get; set; }
-    public string Formacao { get; set; } = null!;
-    public decimal Salario { get; set; }
+    public Guid Id { get; init; }
+    public string Nome { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Telefone { get; init; } = null!;
+    public string Status { get; init; } = null!;
+    public DateOnly Nasc { get; init; }
+    public string Endereco { get; init; } = null!;
+    public string Cpf { get; init; } = null!;
+    public string ContatoEmergencia { get; init; } = null!;
+    public string Registro { get; init; } = null!;
+    public List<string> Turmas { get; init; } = null!;
+    public string Disciplina { get; init; } = null!;
+    public DateOnly Contratacao { get; init; }
+    public string Formacao { get; init; } = null!;
+    public decimal Salario { get; init; }
+
+    public ProfessorDTO(Professor dados)
+    {
+        Id = dados.Id;
+        Nome = dados.Nome;
+        Email = dados.Email;
+        Telefone = dados.Telefone;
+        Status = dados.Status;
+        Nasc = dados.Nasc;
+        Endereco = dados.Endereco;
+        Cpf = dados.Cpf;
+        ContatoEmergencia = dados.ContatoEmergencia;
+        Registro = dados.Registro;
+        Turmas = dados.Turmas;
+        Disciplina = dados.Disciplina;
+        Contratacao = dados.Contratacao;
+        Formacao = dados.Formacao;
+        Salario = dados.Salario;
+    }
 }
