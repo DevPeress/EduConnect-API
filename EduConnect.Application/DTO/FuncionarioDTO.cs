7cc8cc4c6b96fa165f51f8ed092baa814c726822
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 12:47:48 -0300

    Alterando DTO para record e adicionando construtor

--- EduConnect.Application/DTO/FuncionarioDTO.cs
+++ EduConnect.Application/DTO/FuncionarioDTO.cs
@@ -1,21 +1,43 @@
-﻿namespace EduConnect.Application.DTO;
+﻿using EduConnect.Domain.Entities;
 
-public record FuncionarioDTO
+namespace EduConnect.Application.DTO;
+
+public class FuncionarioDTO
 {
-    public Guid Id { get; set; }
-    public string Nome { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Telefone { get; set; } = null!;
-    public string Status { get; set; } = null!;
-    public DateOnly Nasc { get; set; }
-    public string Endereco { get; set; } = null!;
-    public string Cpf { get; set; } = null!;
-    public string ContatoEmergencia { get; set; } = null!;
-    public string Registro { get; set; } = null!;
-    public string Cargo { get; set; } = null!;
-    public DateOnly DataAdmissao { get; set; }
-    public decimal Salario { get; set; }
-    public string Departamento { get; set; } = null!;
-    public string Supervisor { get; set; } = null!;
-    public string Turno { get; set; } = null!;
+    public Guid Id { get; init; }
+    public string Nome { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Telefone { get; init; } = null!;
+    public string Status { get; init; } = null!;
+    public DateOnly Nasc { get; init; }
+    public string Endereco { get; init; } = null!;
+    public string Cpf { get; init; } = null!;
+    public string ContatoEmergencia { get; init; } = null!;
+    public string Registro { get; init; } = null!;
+    public string Cargo { get; init; } = null!;
+    public DateOnly DataAdmissao { get; init; }
+    public decimal Salario { get; init; }
+    public string Departamento { get; init; } = null!;
+    public string Supervisor { get; init; } = null!;
+    public string Turno { get; init; } = null!;
+
+    public FuncionarioDTO(Funcionario dados)
+    {
+        Id = dados.Id;
+        Nome = dados.Nome;
+        Email = dados.Email;
+        Telefone = dados.Telefone;
+        Status = dados.Status;
+        Nasc = dados.Nasc;
+        Endereco = dados.Endereco;
+        Cpf = dados.Cpf;
+        ContatoEmergencia = dados.ContatoEmergencia;
+        Registro = dados.Registro;
+        Cargo = dados.Cargo;
+        DataAdmissao = dados.DataAdmissao;
+        Salario = dados.Salario;
+        Departamento = dados.Departamento;
+        Supervisor = dados.Supervisor;
+        Turno = dados.Turno;
+    }
 }
