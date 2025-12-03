c9881aa506da3b39753b194d84cf1d1dc1ebc84e
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 13:33:07 -0300

    Alterando Registro para Id

--- WebApplication1/Controllers/TurmaController.cs
+++ WebApplication1/Controllers/TurmaController.cs
@@ -34,7 +34,7 @@ namespace EduConnect.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateTurma(int id, TurmaDTO turmaDTO)
         {
-            if (id != turmaDTO.Registro)
+            if (id != turmaDTO.Id)
             {
                 return BadRequest();
             }
