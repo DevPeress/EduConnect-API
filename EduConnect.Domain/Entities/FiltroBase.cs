80f22fd4b4d8519cd30fa5082dca43592c268acd
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:45:09 -0300

    Adicionando o Objeto de FiltroBase

---
+++ EduConnect.Domain/Entities/FiltroBase.cs
@@ -0,0 +1,12 @@
+﻿namespace EduConnect.Domain.Entities;
+public class FiltroBase
+{
+    public int Limit { get; set; } = 6;
+    public int Offset => CalcularOffset();
+    public int Page { get; set; } = 1;
+
+    int CalcularOffset()
+    {
+        return (Page == 1) ? 0 : (Page - 1) * Limit;
+    }
+}
