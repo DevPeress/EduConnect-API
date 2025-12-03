bb327b17a38b271716e762f22395eb18528fa7c8
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:43:58 -0300

    Criando do Objeto turma

---
+++ EduConnect.Domain/Entities/Turma.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EduConnect.Domain.Entities;
+
+public class Turma
+{
+    [Key]
+    public Guid Registro { get; set; }
+    public required string Nome { get; set; }
+    public required string Turno { get; set; }
+    public Guid ProfessorID { get; set; }
+    public List<Guid> Alunos { get; set; } = [];
+    public Guid SalaID { get; set; }
+    public Guid DisciplinaID { get; set; }
+    public required string Horario { get; set; }
+    public int Capacidade { get; set; }
+    public DateOnly AnoLetivo { get; set; }
+}
