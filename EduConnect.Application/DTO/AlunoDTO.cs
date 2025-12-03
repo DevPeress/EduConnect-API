3cd7811956398566ea5cf87c2c1b9fd42398f786
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-03 11:48:09 -0300

    Alterando DTO para record e adicionando construtor

--- EduConnect.Application/DTO/AlunoDTO.cs
+++ EduConnect.Application/DTO/AlunoDTO.cs
@@ -1,17 +1,36 @@
-﻿namespace EduConnect.Application.DTO;
-public class AlunoDTO
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO;
+public record AlunoDTO
 {
-    public Guid Id { get; set; }
-    public string Registro { get; set; } = null!;
-    public string Nome { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Telefone { get; set; } = null!;
-    public string Status { get; set; } = null!;
-    public DateOnly Nasc { get; set; }
-    public string Endereco { get; set; } = null!;
-    public string Cpf { get; set; } = null!;
-    public string ContatoEmergencia { get; set; } = null!;
-    public string Turma { get; set; } = null!;
-    public int Media { get; set; }
-    public DateOnly DataMatricula { get; set; }
+    public Guid Id { get; init; }
+    public string Registro { get; init; } = null!;
+    public string Nome { get; init; } = null!;
+    public string Email { get; init; } = null!;
+    public string Telefone { get; init; } = null!;
+    public string Status { get; init; } = null!;
+    public DateOnly Nasc { get; init; }
+    public string Endereco { get; init; } = null!;
+    public string Cpf { get; init; } = null!;
+    public string ContatoEmergencia { get; init; } = null!;
+    public string Turma { get; init; } = null!;
+    public int Media { get; init; }
+    public DateOnly DataMatricula { get; init; }
+
+    public AlunoDTO(Aluno dados)
+    {
+        Id = dados.Id;
+        Registro = dados.Registro;
+        Nome = dados.Nome;
+        Email = dados.Email;
+        Telefone = dados.Telefone;
+        Status = dados.Status;
+        Nasc = dados.Nasc;
+        Endereco = dados.Endereco;
+        Cpf = dados.Cpf;
+        ContatoEmergencia = dados.ContatoEmergencia;
+        Turma = dados.Turma;
+        Media = dados.Media;
+        DataMatricula = dados.DataMatricula;
+    }
 }
