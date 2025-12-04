77c96daa5a1192d3469a271e33255cbf9aaf30fa
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-04 10:02:05 -0300

    Inserindo na interface a Task de pegar os dados do dashboard

--- EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
+++ EduConnect.Domain/Interfaces/IFinanceiroRepository.cs
@@ -6,6 +6,7 @@ public interface IFinanceiroRepository
 {
     Task<List<Financeiro>> GetAll();
     Task <List<Financeiro>> GetByAlunoId(int alunoId);
+    Task<(decimal TotalRecebido, decimal TotalPendente, decimal TotalAtrasado)> GetDashBoard();
     Task <(IEnumerable<Financeiro>, int TotalRegistro)> GetByFilters(FinanceiroFiltro filtro);
     Task <Financeiro?> GetById(int id);
     Task Add(Financeiro financeiro);
