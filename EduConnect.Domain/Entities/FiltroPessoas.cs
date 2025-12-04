c817c73d7a0ac94afae257807bd067506bff7b20
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-04 08:49:30 -0300

    Adicionando mais tipos no filtro de pessoas

--- EduConnect.Domain/Entities/FiltroPessoas.cs
+++ EduConnect.Domain/Entities/FiltroPessoas.cs
@@ -3,4 +3,6 @@ namespace EduConnect.Domain.Entities;
 public class FiltroPessoas : FiltroBase
 {
     public string? Status { get; set; }
+    public string? Categoria { get; set; }
+    public string? Data { get; set; }
 }
