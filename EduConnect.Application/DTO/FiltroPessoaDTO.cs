39ffb55dd35a0117dbdb52a2e544f1310eb694a3
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-04 08:48:52 -0300

    Criação do Filtro de Pessoas DTO

---
+++ EduConnect.Application/DTO/FiltroPessoaDTO.cs
@@ -0,0 +1,10 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO;
+
+public class FiltroPessoaDTO : FiltroBase
+{
+    public string? Categoria { get; set; }
+    public string? Data { get; set; }
+    public string? Status { get; set; }
+}
