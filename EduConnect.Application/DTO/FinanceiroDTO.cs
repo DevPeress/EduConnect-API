52175357e35e21da910b77a573eb46387c51d491
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-05 10:49:34 -0300

    Adicionando Metodo, Descrição e Obversações ao DTO

--- EduConnect.Application/DTO/FinanceiroDTO.cs
+++ EduConnect.Application/DTO/FinanceiroDTO.cs
@@ -7,16 +7,21 @@ public record FinanceiroDTO
     public int Registro { get; init; }
     public int AlunoId { get; init; }
     public string Categoria { get; init; } = default!;
+    public string Metodo { get; init; } = default!;
+    public string Descricao { get; init; } = default!;
     public decimal Valor { get; init; }
     public DateOnly DataVencimento { get; init; }
     public bool Pago { get; init; }
     public DateOnly? DataPagamento { get; init; }
     public bool Cancelado { get; init; }
+    public string? Observacoes { get; init; }
 
     // Dados opcionais relacionados ao aluno (preenchidos no serviço, não na entidade)
     public string? Aluno { get; init; }
     public DateOnly? Nasc { get; init; }
     public string? Status { get; init; }
+
+    public FinanceiroDTO() { }
 
     public FinanceiroDTO(Financeiro u)
     {
