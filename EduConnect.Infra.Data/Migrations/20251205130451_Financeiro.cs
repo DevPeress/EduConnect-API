ba540c53a56026f3fd7ad6af9a19c11830fbb597
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-05 10:50:27 -0300

    Adicionando migration para alterar os dados novos do financeiro

---
+++ EduConnect.Infra.Data/Migrations/20251205130451_Financeiro.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduConnect.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Financeiro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Financeiros",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Metodo",
+                table: "Financeiros",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Financeiros");
+
+            migrationBuilder.DropColumn(
+                name: "Metodo",
+                table: "Financeiros");
+        }
+    }
+}
