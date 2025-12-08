165d783f4e61012f72c3a616ff9c3096ae7dd43b
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-08 15:15:09 -0300

    Adicionando middleware de jwt

---
+++ WebApplication1/MiddleWares/JWTConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using System.Text;
+
+namespace Cemig.Part2.Middlewares
+{
+    public static class JWTConfiguration
+    {
+        extension(IServiceCollection services)
+        {
+            public IServiceCollection AddJWTConfiguration(IConfiguration configuration)
+            {
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["ISSUER"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]!)),
+                        RequireExpirationTime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if (context.Request.Cookies.ContainsKey("auth"))
+                            {
+                                context.Token = context.Request.Cookies["auth"];
+                            }
+                            return Task.CompletedTask;
+                        },
+                        OnAuthenticationFailed = context =>
+                        {
+                            if (context.Exception is SecurityTokenExpiredException)
+                            {
+                                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                                return context.Response.WriteAsync("Token expirado.");
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+
+                });
+
+                return services;
+            }
+        }
+    }
+}
