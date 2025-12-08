d8397ab900e858168bb34254da7ef1eaaad29ad1
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-08 15:14:51 -0300

    Criação do Serviço de JWT para criar token via httponly

---
+++ EduConnect.Application/Services/JWTService.cs
@@ -0,0 +1,31 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace EduConnect.Application.Services;
+
+public class JWTService(IConfiguration config)
+{
+    private readonly IConfiguration _config = config;
+    public string GenerateToken(int userId, string role, bool lembrar)
+    {
+        var claims = new[]
+        {
+            new Claim("id", userId.ToString()),
+            new Claim(ClaimTypes.Role, role)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtKey"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(lembrar ? 9 : 1),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
