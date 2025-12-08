7ce165cecd21a93130e7c8704bd5b0849d0d0bbf
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-08 16:44:20 -0300

    Adicionando verificação se lembrar existe e o valor dele é verdadeiro ou falso

--- EduConnect.Application/Services/JWTService.cs
+++ EduConnect.Application/Services/JWTService.cs
@@ -9,7 +9,7 @@ namespace EduConnect.Application.Services;
 public class JWTService(IConfiguration config)
 {
     private readonly IConfiguration _config = config;
-    public string GenerateToken(int userId, string role, bool lembrar)
+    public string GenerateToken(int userId, string role, bool? lembrar)
     {
         var claims = new[]
         {
@@ -19,10 +19,11 @@ public class JWTService(IConfiguration config)
 
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtKey"]!));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var tempo = lembrar != null && lembrar == true ? 9 : 1;
 
         var token = new JwtSecurityToken(
             claims: claims,
-            expires: DateTime.UtcNow.AddHours(lembrar ? 9 : 1),
+            expires: DateTime.UtcNow.AddHours(tempo),
             signingCredentials: creds
         );
 
