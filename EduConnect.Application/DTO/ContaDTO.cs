efa20efda3de3194c875d849b8aa164b503b9602
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-15 10:17:16 -0300

    Adicionando Cargo ao ContaDTO

--- EduConnect.Application/DTO/ContaDTO.cs
+++ EduConnect.Application/DTO/ContaDTO.cs
@@ -4,15 +4,17 @@
     {
         public string Registro { get; init; } = null!;
         public string Senha { get; init; } = null!;
+        public string? Cargo { get; init; }
         public bool? Lembrar { get; init; } = false;
 
         public ContaDTO() { }
 
-        public ContaDTO(string registro, string senha, bool lembrar)
+        public ContaDTO(string registro, string senha, bool lembrar, string cargo)
         {
             Registro = registro;
             Senha = senha;
             Lembrar = lembrar;
+            Cargo = cargo;
         }
     }
 }
