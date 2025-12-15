42a90b110ad8d00015f9d1b6df768cc9ef908c44
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-15 15:34:01 -0300

    Crias task GetInfos

--- EduConnect.Domain/Interfaces/IContaRepository.cs
+++ EduConnect.Domain/Interfaces/IContaRepository.cs
@@ -6,6 +6,7 @@ public interface IContaRepository
 {
     Task<Conta?> GetConta(string registro, string senha);
     Task<bool> EmailExistsAsync(string registro);
+    Task<(string nome, string foto)> GetInfos(string cargo, string registro);
     Task<bool> ChancePassword(string registro, string senhaNova);
     Task AddContaAsync(Conta conta);
     Task DeleteContaAsync(int id);
