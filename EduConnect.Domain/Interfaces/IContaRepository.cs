ab3d316190337f35a78a34ecafa32c9b3be0f97c
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-15 10:06:33 -0300

    Trocando Task para Conta?

--- EduConnect.Domain/Interfaces/IContaRepository.cs
+++ EduConnect.Domain/Interfaces/IContaRepository.cs
@@ -4,7 +4,7 @@ namespace EduConnect.Domain.Interfaces;
 
 public interface IContaRepository
 {
-    Task<Conta> GetConta(string registro, string senha);
+    Task<Conta?> GetConta(string registro, string senha);
     Task<bool> EmailExistsAsync(string registro);
     Task AddContaAsync(Conta conta);
     Task DeleteContaAsync(int id);
