d156ed5e2c1546da3ab5a7030c57550f684f4a02
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-16 09:42:31 -0300

    Adicionando Segurança Manager para gerar e verificar senhas criptografadas

---
+++ EduConnect.Infra.CrossCutting.Utils/SegurancaManager.cs
@@ -0,0 +1,15 @@
+﻿namespace EduConnect.Infra.CrossCutting.Utils;
+
+public class SegurancaManager
+{
+    public static string GerarHash(string valor)
+    {
+        string salt = BCrypt.Net.BCrypt.GenerateSalt(10);
+        return BCrypt.Net.BCrypt.HashPassword(valor, salt);
+    }
+
+    public static bool VerificarHash(string valor, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(valor, hash);
+    }
+}
