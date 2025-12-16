ae452b18cde64b3ce0d33d7fa5eb959430245543
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-16 10:37:55 -0300

    Adicionando Middleware para envio de resposta padronizada

---
+++ WebApplication1/MiddleWares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using EduConnect.Infra.CrossCutting.Entities;
+using System.Net;
+using System.Net.Mime;
+
+namespace EduConnect.MiddleWares;
+
+public class ErrorHandlingMiddleware(RequestDelegate next)
+{
+    private readonly RequestDelegate _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        return context.Response.WriteAsJsonAsync(new CustomProblemDetails
+        {
+            Status = (int)HttpStatusCode.InternalServerError,
+            Detail = $@"{exception.Message}. {exception?.InnerException?.Message}",
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+            Instance = context.Request.Path
+        });
+    }
+}
