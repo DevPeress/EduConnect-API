c4c98abfbfcfa8cd2608523b2b018f18b70c92a5
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-16 17:45:07 -0300

    Criando ViewModel de Retorno dos Dados do Financeiro

---
+++ WebApplication1/ViewModels/FinanceiroResponseViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EduConnect.ViewModels;
+
+public class FinanceiroResponseViewModel
+{
+    public int Registro { get; init; }
+    public required string Aluno { get; init; }
+    public required DateOnly Nasc { get; init; }
+    public string Categoria { get; init; }
+    public decimal Valor { get; init; }
+    public DateOnly DataVencimento { get; init; }
+    public DateOnly? DataPagamento { get; init; }
+    public required string Status { get; init; }
+    public required string Mes { get; init; }
+    public required string Foto { get; init; }
+}
