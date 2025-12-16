68e536ce89c37b3460b5a2b4111ada38508737e1
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-16 09:43:47 -0300

    Adicionando ao utils o sistema de serializar e desserializar json

---
+++ EduConnect.Infra.CrossCutting.Utils/SerializacaoHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace EduConnect.Infra.CrossCutting.Utils;
+
+public static class SerializacaoHelper
+{
+    public static string Serializar(this object obj)
+    {
+        if (obj == null) return string.Empty;
+        return JsonSerializer.Serialize(obj, JsonSerializerOptions.Web);
+    }
+
+    public static T? Desserializar<T>(this string obj)
+    {
+        if (string.IsNullOrEmpty(obj)) return default;
+        return JsonSerializer.Deserialize<T>(obj, JsonSerializerOptions.Web);
+    }
+}
