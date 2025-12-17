05a8d2c04a0638a58f39dc367dd7408943347807
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-17 16:44:11 -0300

    Criando interface base dos repositorio de pessoas

---
+++ EduConnect.Domain/Interfaces/IPessoaRepository.cs
@@ -0,0 +1,13 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Domain.Interfaces;
+
+public interface IPessoaRepository <T> where T : class
+{
+    Task<(IEnumerable<T>, int TotalRegistro)> GetByFilters(Filtro filtro);
+    Task<T?> GetByIdAsync(int id);
+    Task<T?> GetLastPessoaAsync();
+    Task AddAsync(T pessoa);
+    Task UpdateAsync(T pessoa);
+    Task DeleteAsync(int id);
+}
