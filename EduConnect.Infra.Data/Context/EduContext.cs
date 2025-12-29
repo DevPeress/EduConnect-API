e444ed9f012fcbe6821a9b34199c6f15e0022706
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-29 11:47:05 -0300

    Criando modelcreating para separar do educontext e deixar o código mais limpo

--- EduConnect.Infra.Data/Context/EduContext.cs
+++ EduConnect.Infra.Data/Context/EduContext.cs
@@ -1,5 +1,5 @@
-﻿using EduConnect.Domain;
-using EduConnect.Domain.Entities;
+﻿using EduConnect.Domain.Entities;
+using EduConnect.Infra.Data.ModelCreating;
 using Microsoft.EntityFrameworkCore;
 
 namespace EduConnect.Infra.Data.Context;
@@ -17,34 +17,6 @@ public class EduContext(DbContextOptions<EduContext> options) : DbContext(option
     {
         base.OnModelCreating(modelBuilder);
 
-        var tipos = new List<Type>
-        {
-            typeof(Aluno),
-            typeof(Professor),
-            typeof(Funcionario)
-        };
-
-        // Impede que existam 2 tipos com a mesma matrícula
-        foreach (var tipo in tipos)
-        {
-            modelBuilder.Entity(tipo)
-                .HasOne(typeof(Conta), "Conta")
-                .WithOne()
-                .HasForeignKey(tipo, "ContaId")
-                .OnDelete(DeleteBehavior.Restrict);
-        }
-
-        // CONFIGURAÇÃO GLOBAL PARA TODOS OS DECIMAIS
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            var decimalProperties = entityType.GetProperties()
-                .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?));
-
-            foreach (var property in decimalProperties)
-            {
-                property.SetPrecision(18);
-                property.SetScale(2);
-            }
-        }
+        modelBuilder.ConfigureModels();
     }
 }
---
+++ EduConnect.Infra.Data/ModelCreating/ModelCreating.cs
@@ -0,0 +1,41 @@
+﻿using EduConnect.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduConnect.Infra.Data.ModelCreating
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void ConfigureModels(this ModelBuilder modelBuilder)
+        {
+            var tipos = new List<Type>
+            {
+                typeof(Aluno),
+                typeof(Professor),
+                typeof(Funcionario)
+            };
+
+            // Impede que existam 2 tipos com a mesma matrícula
+            foreach (var tipo in tipos)
+            {
+                modelBuilder.Entity(tipo)
+                    .HasOne(typeof(Conta), "Conta")
+                    .WithOne()
+                    .HasForeignKey(tipo, "ContaId")
+                    .OnDelete(DeleteBehavior.Restrict);
+            }
+
+            // CONFIGURAÇÃO GLOBAL PARA TODOS OS DECIMAIS
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var decimalProperties = entityType.GetProperties()
+                    .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?));
+
+                foreach (var property in decimalProperties)
+                {
+                    property.SetPrecision(18);
+                    property.SetScale(2);
+                }
+            }
+        }
+    }
+}
