b27ee8f5382aa10b843edbfb390337d2e8296435
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 16:07:40 -0300

    Criação dos atributos de auditoria

---
+++ WebApplication1/Attributes/AuditoriaAtrtribute.cs
@@ -0,0 +1,10 @@
+﻿using EduConnect.Domain.Enums;
+
+namespace EduConnect.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class AuditAttribute(AuditAction action, string entity) : Attribute
+{
+    public AuditAction Action { get; } = action;
+    public string Entity { get; } = entity;
+}
