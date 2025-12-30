e8f814e6d7610ae49663c904896d00d5ce232b2a
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 18:24:46 -0300

    Criando AuditSet para setar as informações no httpcontext

---
+++ EduConnect.Application/Common/Auditing/AuditSet.cs
@@ -0,0 +1,15 @@
+﻿using EduConnect.Domain.Enums;
+using Microsoft.AspNetCore.Http;
+
+namespace EduConnect.Application.Common.Auditing;
+
+public static class AuditSet
+{
+    public static void SetAudit(this HttpContext context, AuditAction action, string entity, object entityId, string details)
+    {
+        context.Items[AuditKeys.Action] = action;
+        context.Items[AuditKeys.Entity] = entity;
+        context.Items[AuditKeys.EntityId] = entityId.ToString();
+        context.Items[AuditKeys.Details] = details;
+    }
+}
