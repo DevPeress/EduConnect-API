444be1798dc1b650665020f5264762eb7c99d26e
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 17:33:49 -0300

    Retirando imports denecessários

--- EduConnect.Application/Services/AuditoriaService.cs
+++ EduConnect.Application/Services/AuditoriaService.cs
@@ -1,15 +1,11 @@
 ﻿using EduConnect.Domain.Entities;
-using EduConnect.Domain.Enums;
 using EduConnect.Domain.Interfaces;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Security.Claims;
 
 namespace EduConnect.Application.Services;
 
 public class AuditoriaService(IAuditRepository repo, IHttpContextAccessor httpContext)
 {
-    private readonly IHttpContextAccessor _httpContext = httpContext;
     private readonly IAuditRepository _auditRepository = repo;
 
     public async Task LogAsync(Registro registro)
