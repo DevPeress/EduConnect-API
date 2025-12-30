d4e038545f898726b98f0bdce61f853d41317a26
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 13:12:47 -0300

    DTO de registro para informações retornaveis para front-end

--- EduConnect.Application/DTO/RegistroDTO.cs
+++ EduConnect.Application/DTO/RegistroDTO.cs
@@ -1,20 +1,10 @@
-﻿using EduConnect.Domain.Entities;
-
-namespace EduConnect.Application.DTO;
+﻿namespace EduConnect.Application.DTO;
 public record RegistroDTO
 {
-    public int Id { get; init; } 
-    public string Tipo { get; init; } = null!;
-    public string Descricao { get; init; } = null!;
-    public DateTime Horario { get; init; } = DateTime.Now;
-    public int PessoaId { get; init; } 
-
-    public RegistroDTO (Registro dados)
-    {
-        Id = dados.Id;
-        Tipo = dados.Tipo;
-        Descricao = dados.Descricao;
-        Horario = dados.Horario;
-        PessoaId = dados.PessoaId;
-    }
+    public string UserName { get; set; } = null!;
+    public string UserRole { get; set; } = null!;
+    public string Action { get; set; } = null!;
+    public string Entity { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
 }
