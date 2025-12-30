6644a9383872576f6904e40024d2addba6680abf
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 16:06:55 -0300

    Repositorio de auditoria para adicionar os dados

---
+++ EduConnect.Infra.Data/Repositories/AuditoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using EduConnect.Domain.Entities;
+using EduConnect.Domain.Interfaces;
+using EduConnect.Infra.Data.Context;
+
+namespace EduConnect.Infra.Data.Repositories;
+
+public class AuditoriaRepository(EduContext context) : IAuditRepository
+{
+    private readonly EduContext _context = context;
+
+    public async Task AddAsync(Registro registro)
+    {
+        _context.Registros.Add(registro);
+        await _context.SaveChangesAsync();
+    }
+}
