1f2003fa877e75e89112f9bb9cc2ee94821a0f97
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 18:16:51 -0300

    Ajustando service para criar o registro

--- EduConnect.Application/Services/AuditoriaService.cs
+++ EduConnect.Application/Services/AuditoriaService.cs
@@ -1,14 +1,31 @@
-﻿using EduConnect.Domain.Entities;
+﻿using EduConnect.Application.Common.Auditing;
+using EduConnect.Domain.Entities;
+using EduConnect.Domain.Enums;
 using EduConnect.Domain.Interfaces;
 
 namespace EduConnect.Application.Services;
 
-public class AuditoriaService(IAuditRepository repo)
+public class AuditoriaService(IAuditContext context, IAuditRepository repo)
 {
-    private readonly IAuditRepository _auditRepository = repo;
+    private readonly IAuditContext _context = context;
+    private readonly IAuditRepository _repo = repo;
 
-    public async Task LogAsync(Registro registro)
+    public async Task LogAsync(AuditAction action, string entity, string entityId, string detalhes)
     {
-        await _auditRepository.AddAsync(registro);
+        var registro = new Registro
+        {
+            UserId = _context.UserId,
+            UserName = _context.UserName,
+            UserRole = _context.UserRole,
+            IpAddress = _context.IpAddress,
+
+            Action = action,
+            Entity = entity,
+            EntityId = entityId,
+            Detalhes = detalhes,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        await _repo.AddAsync(registro);
     }
 }
