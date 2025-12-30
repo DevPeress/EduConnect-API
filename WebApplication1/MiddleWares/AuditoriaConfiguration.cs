27f875e16a468256edeeff64525b42731c5974a9
Fabrício <fabricioperesdsantos@gmail.com>
2025-12-30 16:07:25 -0300

    Middleware de auditoria

---
+++ WebApplication1/MiddleWares/AuditoriaConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using EduConnect.Application.Services;
+using EduConnect.Attributes;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EduConnect.MiddleWares;
+
+public class AuditoriaConfiguration(AuditoriaService service)
+{
+    private readonly AuditoriaService _service = service;
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var executedContext = await next();
+
+        if (executedContext != null)
+            return;
+
+        var auditAttr = context.ActionDescriptor.EndpointMetadata
+            .OfType<AuditAttribute>()
+            .FirstOrDefault();
+
+        if (auditAttr == null)
+            return;
+
+        var entityId = context.ActionArguments
+            .FirstOrDefault().Value?.ToString() ?? "-";
+
+        await _service.LogAsync(
+            auditAttr.Action,
+            auditAttr.Entity,
+            entityId,
+            "Ação executada com sucesso");
+    }
+}
