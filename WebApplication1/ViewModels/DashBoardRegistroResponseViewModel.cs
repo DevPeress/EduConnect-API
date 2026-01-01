f8a3a2742db91537ff35d252e9be32968111cda7
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 14:56:08 -0300

    Criando DashBoard de Registro ViewModel

---
+++ WebApplication1/ViewModels/DashBoardRegistroResponseViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EduConnect.ViewModels;
+
+public class DashBoardRegistroResponseViewModel
+{
+    public required string Tipo { get; set; }
+    public required string Dado { get; set; }
+    public int Horario { get; set; }
+}
