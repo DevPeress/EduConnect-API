7c49931e2328989d607c9b868f6da29ddfa747c7
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 19:10:38 -0300

    SP para totais de pessoas para dashboard

---
+++ EduConnect.Infra.Data/StoredProcedures/DashBoardAdmin/sp_GetTotal.sql
@@ -0,0 +1,20 @@
+CREATE OR ALTER PROCEDURE sp_GetTotal
+AS
+BEGIN 
+    SET NOCOUNT ON;
+
+	SELECT 
+		ISNULL((SELECT COUNT(*) FROM Alunos WHERE Deletado = 0), 0) AS TotalAlunos,
+		ISNULL((SELECT COUNT(*) FROM Professores WHERE Deletado = 0), 0) AS TotalProfessores,
+		ISNULL((SELECT COUNT(*) FROM Turmas WHERE Deletado = 0), 0) AS TotalTurmas,
+		ISNULL((
+            SELECT 
+                SUM(
+                    CASE 
+                        WHEN Presente = 0 AND Justificada = 0 THEN 0
+                        ELSE 1
+                    END
+                )
+            FROM Presencas
+        ), 0) AS TotalPresencas
+END
