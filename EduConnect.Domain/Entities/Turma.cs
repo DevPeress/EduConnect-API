3de5076a82e315ca123aadc064778882cbafb141
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 12:40:34 -0300

    Atualizando turma para relacionamento

--- EduConnect.Domain/Entities/Turma.cs
+++ EduConnect.Domain/Entities/Turma.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EduConnect.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace EduConnect.Domain.Entities;
 
@@ -7,15 +8,17 @@ public class Turma
     [Key]
     public int Registro { get; set; }
     public required string Nome { get; set; }
-    public required string Turno { get; set; }
-    public int ProfessorID { get; set; }
-    public List<int> Alunos { get; set; } = [];
-    public int SalaID { get; set; }
-    public int DisciplinaID { get; set; }
+    public Turno Turno { get; set; }
     public required string Horario { get; set; }
     public int Capacidade { get; set; }
     public DateOnly AnoLetivo { get; set; }
-    public DateOnly DataCriacao { get; set; } = DateOnly.FromDateTime(DateTime.Today);
-    public required string Status { get; set; }
+    public DateOnly DataCriacao { get; set; } 
+    public StatusTurma Status { get; set; }
     public bool Deletado { get; set; } = false;
+
+    // Relacionamento
+    public int ProfessorId { get; set; }
+    public Professor Professor { get; set; } = null!;
+    public ICollection<Aluno> Alunos { get; set; } = [];
+    public int DisciplinaID { get; set; }
 }
