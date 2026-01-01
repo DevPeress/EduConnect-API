1fdcbbeb19e8e41d74a028f93fc9c323e88c512a
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 12:40:25 -0300

    Criando a entidade de presença

---
+++ EduConnect.Domain/Entities/Presenca.cs
@@ -0,0 +1,20 @@
+﻿namespace EduConnect.Domain.Entities;
+
+public class Presenca
+{
+    public int Id { get; set; }
+
+    public int AlunoId { get; set; }
+    public int TurmaId { get; set; }
+    public int DisciplinaId { get; set; }
+
+    public DateOnly Data { get; set; }
+    public bool Presente { get; set; }
+
+    public bool Justificada { get; set; }
+    public string? Observacao { get; set; }
+
+    public int ProfessorId { get; set; }
+
+    public DateTime CriadoEm { get; set; }
+}
