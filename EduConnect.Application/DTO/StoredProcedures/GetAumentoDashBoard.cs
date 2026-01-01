844875342200ddb282b121ef377c140f0f804d2e
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 19:21:55 -0300

    Criando SP para aumento

---
+++ EduConnect.Application/DTO/StoredProcedures/GetAumentoDashBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace EduConnect.Application.DTO.StoredProcedures;
+
+public class GetAumentoDashBoard
+{
+    public int AumentoAlunos { get; set; }
+    public int AumentoProfessores { get; set; }
+    public int AumentoTurmas { get; set; }
+    public int AumentoPresencas { get; set; }
+}
