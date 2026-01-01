4531593649a670322371400a8d51e8da135ca903
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 12:49:35 -0300

    Ajuste do DTO baseado na entidade

--- EduConnect.Application/DTO/TurmaDTO.cs
+++ EduConnect.Application/DTO/TurmaDTO.cs
@@ -1,35 +1,37 @@
 ﻿using EduConnect.Domain.Entities;
+using EduConnect.Domain.Enums;
 
 namespace EduConnect.Application.DTO;
 public record TurmaDTO
 {
     public int Registro { get; set; }
     public required string Nome { get; set; }
-    public required string Turno { get; set; }
-    public int ProfessorID { get; set; }
-    public List<int> Alunos { get; set; } = [];
-    public int SalaID { get; set; }
-    public int DisciplinaID { get; set; }
+    public Turno Turno { get; set; }
     public required string Horario { get; set; }
     public int Capacidade { get; set; }
     public DateOnly AnoLetivo { get; set; }
     public DateOnly DataCriacao { get; set; }
-    public string Status { get; set; } = "Ativa";
+    public StatusTurma Status { get; set; }
+    public bool Deletado { get; set; } 
+    public int ProfessorID { get; set; }
+    public ICollection<Aluno> Alunos { get; set; } = [];
+    public int DisciplinaID { get; set; }
 
     public TurmaDTO() { }
 
     public TurmaDTO(Turma dados)
     {
+        Registro = dados.Registro;
         Nome = dados.Nome;
         Turno = dados.Turno;
-        ProfessorID = dados.ProfessorID;
-        Alunos = dados.Alunos;
-        SalaID = dados.SalaID;
-        DisciplinaID = dados.DisciplinaID;
         Horario = dados.Horario;
         Capacidade = dados.Capacidade;
         AnoLetivo = dados.AnoLetivo;
         DataCriacao = dados.DataCriacao;
         Status = dados.Status;
+        Deletado = dados.Deletado;
+        ProfessorID = dados.ProfessorId;
+        Alunos = dados.Alunos;
+        DisciplinaID = dados.DisciplinaID;
     }
 }
