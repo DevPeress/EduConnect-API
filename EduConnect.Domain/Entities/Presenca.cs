2fdacefbf49eb4395586ca175035c17e93d14bad
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-01 13:20:30 -0300

    Ajustando a entidade de presença para ter relacionamento baseado no aluno e turma e necessário ter ambos para registrar uma

--- EduConnect.Domain/Entities/Presenca.cs
+++ EduConnect.Domain/Entities/Presenca.cs
@@ -3,18 +3,16 @@
 public class Presenca
 {
     public int Id { get; set; }
-
-    public int AlunoId { get; set; }
-    public int TurmaId { get; set; }
-    public int DisciplinaId { get; set; }
-
     public DateOnly Data { get; set; }
     public bool Presente { get; set; }
-
     public bool Justificada { get; set; }
     public string? Observacao { get; set; }
-
     public int ProfessorId { get; set; }
-
     public DateTime CriadoEm { get; set; }
+
+    // Relacionamento
+    public int AlunoId { get; set; }
+    public required Aluno Aluno { get; set; }
+    public int TurmaId { get; set; }
+    public required Turma Turma { get; set; }
 }
