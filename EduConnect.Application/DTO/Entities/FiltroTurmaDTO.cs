3b7af60168455e25b956fc3ce4ba88262a92ff90
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-02 11:33:39 -0300

    Criando FiltroTurmaDTO

---
+++ EduConnect.Application/DTO/Entities/FiltroTurmaDTO.cs
@@ -0,0 +1,10 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO.Entities;
+
+public class FiltroTurmaDTO : FiltroBase
+{
+    public required string Ano { get; set; }
+    public required string Turno { get; set; }
+    public required string Status { get; set; }
+}
