6788aae2e0af32b828a0d12c36049e5d7ed7f9a0
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-02 11:34:36 -0300

    Criação da Entidade de Filtro de Turma

---
+++ EduConnect.Domain/Entities/FiltroTurma.cs
@@ -0,0 +1,8 @@
+﻿
+namespace EduConnect.Domain.Entities;
+public class FiltroTurma : FiltroBase
+{
+    public required string Ano { get; set; }
+    public required string Turno { get; set; }
+    public required string Status { get; set; }
+}
