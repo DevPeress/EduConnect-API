05868574103530b54f052b005797765f73364f09
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-02 10:11:20 -0300

    Adicionando ano ao filtro

--- EduConnect.Domain/Entities/Filtro.cs
+++ EduConnect.Domain/Entities/Filtro.cs
@@ -2,6 +2,7 @@
 namespace EduConnect.Domain.Entities;
 public class Filtro : FiltroBase
 {
+    public string? Ano { get; set; }
     public string? Status { get; set; }
     public string? Categoria { get; set; }
     public string? Data { get; set; }
