139201587a1896e6dd25faa50622d269aef7a2f3
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-02 11:09:07 -0300

    Adicionando ano ao filtrodto

--- EduConnect.Application/DTO/Entities/FiltroDTO.cs
+++ EduConnect.Application/DTO/Entities/FiltroDTO.cs
@@ -4,6 +4,7 @@ namespace EduConnect.Application.DTO.Entities;
 
 public class FiltroDTO : FiltroBase
 {
+    public string? Ano { get; set; }
     public string? Categoria { get; set; }
     public string? Data { get; set; }
     public string? Status { get; set; }
