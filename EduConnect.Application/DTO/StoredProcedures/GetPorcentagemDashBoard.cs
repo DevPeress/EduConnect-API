12918194540a0eef3e5b346dbeb7fff5af42f791
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-02 09:59:34 -0300

    Criação do DTO de GetPorcentagens

---
+++ EduConnect.Application/DTO/StoredProcedures/GetPorcentagemDashBoard.cs
@@ -0,0 +1,13 @@
+﻿namespace EduConnect.Application.DTO.StoredProcedures;
+
+public class GetPorcentagemDashBoard
+{
+    public int AumentoAluno { get; set; }
+    public int DecrementoAluno { get; set; }
+    public int AumentoProfessor { get; set; }
+    public int DecrementoProfessor { get; set; }
+    public int AumentoTurma { get; set; }
+    public int DecrementoTurma { get; set; }
+    public int AumentoPresenca { get; set; }
+    public int DecrementoPresenca { get; set; }
+}
