2b7c91550171162529c8d193b9ce8c50745bf3f5
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-02 09:58:54 -0300

    SP para dashboard de administrador

--- EduConnect.Infra.Data/StoredProcedures/DashBoardAdmin/sp_GetAumento.sql
+++ EduConnect.Infra.Data/StoredProcedures/DashBoardAdmin/sp_GetAumento.sql
@@ -1,31 +1,32 @@
 ﻿CREATE OR ALTER PROCEDURE sp_GetAumentoDashBoard
     @MesAtual INT,
-    @MesAnterior INT
+    @MesAnterior INT,
+    @Ano INT
 AS
 BEGIN 
     SET NOCOUNT ON;
 
-	-- Alunos
+    -- Alunos
     DECLARE @AumentoAlunos INT;
     SELECT @AumentoAlunos = 
-        ISNULL(COUNT(CASE WHEN MONTH(DataMatricula) = @MesAtual THEN 1 END), 0) -
-        ISNULL(COUNT(CASE WHEN MONTH(DataMatricula) = @MesAnterior THEN 1 END), 0)
+        ISNULL(COUNT(CASE WHEN MONTH(DataMatricula) = @MesAtual AND YEAR(DataMatricula) = @Ano THEN 1 END), 0) -
+        ISNULL(COUNT(CASE WHEN MONTH(DataMatricula) = @MesAnterior AND YEAR(DataMatricula) = @Ano THEN 1 END), 0)
     FROM Alunos
     WHERE Deletado = 0;
 
     -- Professores
     DECLARE @AumentoProfessores INT;
     SELECT @AumentoProfessores = 
-        ISNULL(COUNT(CASE WHEN MONTH(Contratacao) = @MesAtual THEN 1 END), 0) -
-        ISNULL(COUNT(CASE WHEN MONTH(Contratacao) = @MesAnterior THEN 1 END), 0)
+        ISNULL(COUNT(CASE WHEN MONTH(Contratacao) = @MesAtual AND YEAR(Contratacao) = @Ano THEN 1 END), 0) -
+        ISNULL(COUNT(CASE WHEN MONTH(Contratacao) = @MesAnterior AND YEAR(Contratacao) = @Ano THEN 1 END), 0)
     FROM Professores
     WHERE Deletado = 0;
 
     -- Turmas
     DECLARE @AumentoTurmas INT;
     SELECT @AumentoTurmas = 
-        ISNULL(COUNT(CASE WHEN MONTH(DataCriacao) = @MesAtual THEN 1 END), 0) -
-        ISNULL(COUNT(CASE WHEN MONTH(DataCriacao) = @MesAnterior THEN 1 END), 0)
+        ISNULL(COUNT(CASE WHEN MONTH(DataCriacao) = @MesAtual AND YEAR(DataCriacao) = @Ano THEN 1 END), 0) -
+        ISNULL(COUNT(CASE WHEN MONTH(DataCriacao) = @MesAnterior AND YEAR(DataCriacao) = @Ano THEN 1 END), 0)
     FROM Turmas
     WHERE Deletado = 0;
 
@@ -36,13 +37,13 @@ BEGIN
     DECLARE @PresencasMesAtual INT;
     SELECT @PresencasMesAtual = ISNULL(COUNT(*), 0)
     FROM Presencas
-    WHERE (Presente = 1 OR Justificada = 1) AND MONTH(Data) = @MesAtual;
+    WHERE (Presente = 1 OR Justificada = 1) AND MONTH(Data) = @MesAtual AND YEAR(Data) = @Ano;
 
     -- Total presencas do mes anterior que devem ser decrementadas
     DECLARE @DecrementoPresencas INT;
     SELECT @DecrementoPresencas = ISNULL(COUNT(*), 0)
     FROM Presencas
-    WHERE (Presente = 0 AND Justificada = 0) AND MONTH(Data) = @MesAnterior;
+    WHERE (Presente = 0 AND Justificada = 0) AND MONTH(Data) = @MesAnterior AND YEAR(Data) = @Ano;
 
     SET @AumentoPresencas = @PresencasMesAtual - @DecrementoPresencas;
 
---
+++ EduConnect.Infra.Data/StoredProcedures/DashBoardAdmin/sp_GetPorcentagem.sql
@@ -0,0 +1,65 @@
+﻿CREATE PROCEDURE sp_GetPorcentagem
+    @MesAtual INT,
+    @MesAnterior INT,
+    @Ano INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Alunos
+    DECLARE @AumentoAluno INT;
+    DECLARE @DecrementoAluno INT;
+    SELECT 
+        @AumentoAluno = COUNT(*) 
+    FROM Alunos 
+    WHERE Deletado = 0 AND MONTH(DataMatricula) = @MesAtual AND YEAR(DataMatricula) = @Ano;
+
+    SELECT 
+        @DecrementoAluno = COUNT(*) 
+    FROM Alunos 
+    WHERE Deletado = 0 AND MONTH(DataMatricula) = @MesAnterior AND YEAR(DataMatricula) = @Ano;
+
+    -- Professores
+    DECLARE @AumentoProfessor INT;
+    DECLARE @DecrementoProfessor INT;
+    SELECT @AumentoProfessor = COUNT(*) 
+    FROM Professores 
+    WHERE Deletado = 0 AND MONTH(Contratacao) = @MesAtual AND YEAR(Contratacao) = @Ano;
+
+    SELECT @DecrementoProfessor = COUNT(*) 
+    FROM Professores 
+    WHERE Deletado = 0 AND MONTH(Contratacao) = @MesAnterior AND YEAR(Contratacao) = @Ano;
+
+    -- Turmas
+    DECLARE @AumentoTurma INT;
+    DECLARE @DecrementoTurma INT;
+    SELECT @AumentoTurma = COUNT(*) 
+    FROM Turmas 
+    WHERE MONTH(DataCriacao) = @MesAtual AND YEAR(DataCriacao) = @Ano;
+
+    SELECT @DecrementoTurma = COUNT(*) 
+    FROM Turmas 
+    WHERE MONTH(DataCriacao) = @MesAnterior AND YEAR(DataCriacao) = @Ano;
+
+    -- Presenças
+    DECLARE @AumentoPresenca INT;
+    DECLARE @DecrementoPresenca INT;
+    SELECT @AumentoPresenca = COUNT(*) 
+    FROM Presencas 
+    WHERE (Presente = 1 OR Justificada = 1) AND MONTH(Data) = @MesAtual AND YEAR(Data) = @Ano;
+
+    SELECT @DecrementoPresenca = COUNT(*) 
+    FROM Presencas 
+    WHERE (Presente = 0 AND Justificada = 0) AND MONTH(Data) = @MesAnterior AND YEAR(Data) = @Ano;
+
+    -- Retornar todas as contagens em uma linha
+    SELECT 
+        @AumentoAluno AS AumentoAluno,
+        @DecrementoAluno AS DecrementoAluno,
+        @AumentoProfessor AS AumentoProfessor,
+        @DecrementoProfessor AS DecrementoProfessor,
+        @AumentoTurma AS AumentoTurma,
+        @DecrementoTurma AS DecrementoTurma,
+        @AumentoPresenca AS AumentoPresenca,
+        @DecrementoPresenca AS DecrementoPresenca;
+END
