da5a3021d6ac07527de3d0422c45dd97a9ca8ba5
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 11:35:35 -0300

    Criação da entidade de filtro para financeiro

---
+++ EduConnect.Domain/Entities/FiltroFinanceiro.cs
@@ -0,0 +1,9 @@
+﻿
+namespace EduConnect.Domain.Entities;
+public class FiltroFinanceiro : FiltroBase
+{
+    public required string Status { get; set; }
+    public required string Pagamento { get; set; }
+    public required string Categoria { get; set; }
+    public required string Meses { get; set; }
+}
