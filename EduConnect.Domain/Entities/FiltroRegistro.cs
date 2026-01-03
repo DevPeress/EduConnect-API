a77da099960fba33a2a14354efa209508951d902
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 11:31:48 -0300

    Criação da Entidade de filtro para registro

---
+++ EduConnect.Domain/Entities/FiltroRegistro.cs
@@ -0,0 +1,8 @@
+﻿
+namespace EduConnect.Domain.Entities;
+public class FiltroRegistro : FiltroBase
+{
+    public required string Ano { get; set; }
+    public required string Categoria { get; set; }
+    public required string Status { get; set; }
+}
