bcd496b7f0be1db746ba1975c7c76db00df07dd3
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 11:31:38 -0300

    Criando DTO para Filtro de Registro

---
+++ EduConnect.Application/DTO/Entities/FiltroRegistroDTO.cs
@@ -0,0 +1,10 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO.Entities;
+
+public class FiltroRegistroDTO : FiltroBase
+{
+    public required string Ano { get; set; }
+    public required string Categoria { get; set; }
+    public required string Status { get; set; }
+}
