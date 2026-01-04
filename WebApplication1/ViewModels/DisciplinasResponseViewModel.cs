030cbc97f577f41e9b10f56f93ee48379cbdf9a3
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 20:46:54 -0300

    Criando response view de disciplinas

---
+++ WebApplication1/ViewModels/DisciplinasResponseViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EduConnect.ViewModels;
+
+public class DisciplinasResponseViewModel
+{
+    public required string Registro { get; set; }
+    public required string Nome { get; set; }
+}
