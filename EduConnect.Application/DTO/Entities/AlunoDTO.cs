1dda298ef8291fb55d363db5015a97739e96543c
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 11:15:38 -0300

    Voltando ao padrão

--- EduConnect.Application/DTO/Entities/AlunoDTO.cs
+++ EduConnect.Application/DTO/Entities/AlunoDTO.cs
@@ -1,15 +1,8 @@
 ﻿using EduConnect.Domain.Entities;
 
 namespace EduConnect.Application.DTO.Entities;
-public class AlunoDTO 
+public class AlunoDTO : PessoaDTO
 {
-    public required string Registro { get; init; }
-    public required string Nome { get; init; }
-    public required string Email { get; init; }
-    public required string Foto { get; init; }
-    public required string Telefone { get; init; }
-    public required string Status { get; init; }
-    public required DateOnly Nasc { get; init; }
     public string? TurmaRegistro { get; init; } 
     public AlunoDTO() { }
     public AlunoDTO(Aluno dados)
