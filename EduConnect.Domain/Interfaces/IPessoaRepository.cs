85b8f1b8c648926b170689e7c424de5676b380e2
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 10:36:42 -0300

    Trocando int id para string registro para procurar pelo registro e não id criado pelo ef

--- EduConnect.Domain/Interfaces/IPessoaRepository.cs
+++ EduConnect.Domain/Interfaces/IPessoaRepository.cs
@@ -6,9 +6,9 @@ public interface IPessoaRepository <T> where T : class
 {
     Task<(IEnumerable<T>, int TotalRegistro)> GetByFilters(FiltroPessoa filtro);
     Task<(List<string>, List<string>?)> GetInformativos();
-    Task<T?> GetByIdAsync(int id);
+    Task<T?> GetByIdAsync(string Registro);
     Task<T?> GetLastPessoaAsync();
     Task AddAsync(T pessoa);
     Task UpdateAsync(T pessoa);
-    Task DeleteAsync(int id);
+    Task DeleteAsync(string Registro);
 }
