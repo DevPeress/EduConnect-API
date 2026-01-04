f9afa6fb94704e23fb0a080efd057b9c8e7b46b6
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 11:11:05 -0300

    Ajustando DTO de Aluno para ter os proprios dados

--- EduConnect.Application/DTO/Entities/AlunoDTO.cs
+++ EduConnect.Application/DTO/Entities/AlunoDTO.cs
@@ -1,8 +1,15 @@
 ﻿using EduConnect.Domain.Entities;
 
 namespace EduConnect.Application.DTO.Entities;
-public class AlunoDTO : PessoaDTO
+public class AlunoDTO 
 {
+    public required string Registro { get; init; }
+    public required string Nome { get; init; }
+    public required string Email { get; init; }
+    public required string Foto { get; init; }
+    public required string Telefone { get; init; }
+    public required string Status { get; init; }
+    public required DateOnly Nasc { get; init; }
     public string? TurmaRegistro { get; init; } 
     public AlunoDTO() { }
     public AlunoDTO(Aluno dados)
