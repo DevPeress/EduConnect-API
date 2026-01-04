98a7a88bc7db92eb15681c35d378879090424284
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 10:16:59 -0300

    Trocando DTO de turma para TurmaCadastro

---
+++ EduConnect.Application/DTO/Entities/TurmaCadastroDTO.cs
@@ -0,0 +1,18 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO.Entities;
+public record TurmaCadastroDTO
+{
+    public required string Registro { get; set; }
+    public required string Nome { get; set; }
+    public required string Turno { get; set; }
+    public required string Status { get; set; }
+    public required DateOnly AnoEletivo { get; set; }
+    public required int Capacidade { get; set; }
+    public required int ProfessorResponsavel { get; set; }
+    public required DateTime Inicio { get; set; }
+    public required DateTime Fim { get; set; }
+    public required string Sala { get; set; }
+    public required string Dias { get; set; }
+    public required ICollection<Disciplinas> Disciplinas { get; set; }
+}
