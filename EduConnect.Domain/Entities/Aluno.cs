9577eea77362165b06e8b43ae770a96c1c72b8c8
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 21:56:35 -0300

    Adicionando dependencia de ter uma turma criada para poder criar um aluno

--- EduConnect.Domain/Entities/Aluno.cs
+++ EduConnect.Domain/Entities/Aluno.cs
@@ -6,7 +6,10 @@ public class Aluno : Pessoa, IPessoaComConta
 {
     public int ContaId { get; set; }
     public Conta Conta { get; set; } = null!;
-    public required string Turma { get; set; }
     public required int Media { get; set; }
     public required DateOnly DataMatricula { get; set; }
+
+    // Relacionamento
+    public required string TurmaId { get; set; }
+    public Turma Turma { get; set; } = null!;
 }
