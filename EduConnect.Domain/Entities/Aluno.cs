7ad6ff394342631f8d5cb72f00434392d8ef331c
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 23:12:42 -0300

    Ajuste de relacionamento

--- EduConnect.Domain/Entities/Aluno.cs
+++ EduConnect.Domain/Entities/Aluno.cs
@@ -4,12 +4,12 @@ namespace EduConnect.Domain.Entities;
 
 public class Aluno : Pessoa, IPessoaComConta
 {
-    public int ContaId { get; set; }
-    public Conta Conta { get; set; } = null!;
     public required int Media { get; set; }
     public required DateOnly DataMatricula { get; set; }
 
     // Relacionamento
-    public required string TurmaId { get; set; }
-    public Turma Turma { get; set; } = null!;
+    public int ContaId { get; set; }
+    public Conta Conta { get; set; } = null!;
+    public string? TurmaId { get; set; }
+    public Turma? Turma { get; set; } = null!;
 }
