5a0711d8ceeb434f3663368e841a0db062b6aa34
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 23:01:18 -0300

    Adicionando required

--- WebApplication1/ViewModels/CardsAdminResponseViewModel.cs
+++ WebApplication1/ViewModels/CardsAdminResponseViewModel.cs
@@ -2,7 +2,7 @@
 
 public class CardsAdminResponseViewModel
 {
-    public string Dado { get; set; }
+    public required string Dado { get; set; }
     public int Total { get; set; }
     public int Aumento { get; set; }
     public double Porcentagem { get; set; }
