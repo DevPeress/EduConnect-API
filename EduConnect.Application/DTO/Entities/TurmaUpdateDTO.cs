cc2eb09b728c57ee6a7922ed698c0004bce7d854
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 10:20:47 -0300

    Criando DTO para Update de Turma

---
+++ EduConnect.Application/DTO/Entities/TurmaUpdateDTO.cs
@@ -0,0 +1,21 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO.Entities;
+
+public record TurmaUpdateDTO
+{
+    public required string Registro { get; set; }
+    public required string Nome { get; set; }
+    public required string Turno { get; set; }
+    public required string Status { get; set; }
+    public required DateOnly AnoEletivo { get; set; }
+    public required int Capacidade { get; set; }
+    public required int ProfessorResponsavel { get; set; }
+    public required DateTime Inicio { get; set; }
+    public required DateTime Fim { get; set; }
+    public required string Sala { get; set; }
+    public required string Dias { get; set; }
+    public required bool Deletado { get; set; } 
+    public required ICollection<TurmaDisciplina> TurmaDisciplina { get; set; }
+    public required ICollection<Aluno> Alunos { get; set; }
+}
