b4cba6fe3617bd2f874cb104082a73be2bfa2476
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 14:37:00 -0300

    Retirando !.value

--- EduConnect.Infra.Data/Repositories/AlunoRepository.cs
+++ EduConnect.Infra.Data/Repositories/AlunoRepository.cs
@@ -17,7 +17,7 @@ public class AlunoRepository(EduContext context) : IAlunoRepository
         if (filtro.Ano != null && filtro.Ano != "Todos os Anos")
         {
             int anoLetivo = int.Parse(filtro.Ano);
-            query = query.Where(dados => dados.DataMatricula!.Value.Year == anoLetivo);
+            query = query.Where(dados => dados.DataMatricula.Year == anoLetivo);
         }
         
         if (filtro.Status != null && filtro.Status != "Todos os Status")
@@ -48,7 +48,7 @@ public class AlunoRepository(EduContext context) : IAlunoRepository
     {
         var anos = await _context.Alunos
             .Where(a => a.Deletado == false)
-            .Select(a => a.DataMatricula!.Value.Year.ToString())
+            .Select(a => a.DataMatricula.Year.ToString())
             .Distinct()
             .ToListAsync();
         var salas = await _context.Alunos
