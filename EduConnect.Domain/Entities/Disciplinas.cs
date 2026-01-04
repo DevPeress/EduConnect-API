8ad41e8e37421eeac6aa07039790ee79908ccbd7
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 22:56:01 -0300

    Criando a entidade de disciplinas

---
+++ EduConnect.Domain/Entities/Disciplinas.cs
@@ -0,0 +1,13 @@
+﻿namespace EduConnect.Domain.Entities;
+
+public class Disciplinas
+{
+    public int DisciplinaID { get; set; }
+    public required string Nome { get; set; }
+    public required string Descricao { get; set; }
+    public DateOnly DataCriacao { get; set; }
+    public bool Deletado { get; set; } = false;
+
+    // Relacionamento
+    public ICollection<Professor> Professores { get; set; } = [];
+}
