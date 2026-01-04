953514f67adf8d269b974cb9d4d119c94d0f9687
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 10:24:11 -0300

    Ajustando relacionamento de Turma

--- EduConnect.Domain/Entities/TurmaDisciplina.cs
+++ EduConnect.Domain/Entities/TurmaDisciplina.cs
@@ -6,7 +6,9 @@ public class TurmaDisciplina
 {
     [Key]
     public int Id { get; set; }
-    public int TurmaRegistro { get; set; }
+
+    // Relacionamentos
+    public string TurmaRegistro { get; set; } = null!;
     public Turma Turma { get; set; } = null!;
 
     public string DisciplinaRegistro { get; set; } = null!;
