35614a0444eecdb5dbd4e4887831318615c5b081
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 10:36:09 -0300

    Criação do DTO de Update de Aluno

---
+++ EduConnect.Application/DTO/Entities/AlunoUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using EduConnect.Domain.Entities;
+
+namespace EduConnect.Application.DTO.Entities;
+
+public record AlunoUpdateDTO
+{
+    public required string Registro { get; init; }
+    public required string Nome { get; init; }
+    public required string Email { get; init; }
+    public required string Foto { get; init; }
+    public required string Telefone { get; init; }
+    public required string Status { get; init; }
+    public required DateOnly Nasc { get; init; }
+    public required string Endereco { get; init; }
+    public required string Cpf { get; init; }
+    public required string ContatoEmergencia { get; init; }
+    public bool Deletado { get; init; }
+    public required int Media { get; init; }
+    public string? TurmaRegistro { get; init; }
+}
