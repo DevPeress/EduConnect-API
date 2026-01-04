2e1a49d642bfd842867da14af79da9ba473731d2
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-03 21:57:27 -0300

    Adicionando relacionamento com turmas

--- EduConnect.Domain/Entities/Professor.cs
+++ EduConnect.Domain/Entities/Professor.cs
@@ -6,9 +6,11 @@ public class Professor : Pessoa, IPessoaComConta
 {
     public int ContaId { get; set; }
     public Conta Conta { get; set; } = null!;
-    public required List<string> Turmas { get; set; }
     public required string Disciplina { get; set; }
     public required DateOnly Contratacao { get; set; }
     public required string Formacao { get; set; }
     public required decimal Salario { get; set; } = decimal.Zero;
+
+    // Relacionamento
+    public ICollection<Turma> Turmas { get; set; } = [];
 }
