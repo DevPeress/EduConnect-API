0b68d780c933e6a85eb84699bb5a1385c1e30672
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-04 10:45:12 -0300

    Ajuste de Relacionamento

--- EduConnect.Domain/Entities/Professor.cs
+++ EduConnect.Domain/Entities/Professor.cs
@@ -4,13 +4,13 @@ namespace EduConnect.Domain.Entities;
 
 public class Professor : Pessoa, IPessoaComConta
 {
-    public int ContaId { get; set; }
-    public Conta Conta { get; set; } = null!;
     public required DateOnly Contratacao { get; set; }
     public required string Formacao { get; set; }
     public required decimal Salario { get; set; } = decimal.Zero;
 
     // Relacionamento
+    public int ContaId { get; set; }
+    public Conta Conta { get; set; } = null!;
     public ICollection<ProfessorDisciplina> ProfessorDisciplinas { get; set; } = [];
     public ICollection<Turma> Turmas { get; set; } = [];
 }
