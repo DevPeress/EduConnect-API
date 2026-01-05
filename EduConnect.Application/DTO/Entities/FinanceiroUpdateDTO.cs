837993c5a9173c2fe1aad7254177c6edce6895d5
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-05 10:46:39 -0300

    Criação do DTO para update de financeiro

---
+++ EduConnect.Application/DTO/Entities/FinanceiroUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace EduConnect.Application.DTO.Entities;
+
+public record FinanceiroUpdateDTO
+{
+    public required string Registro { get; init; }
+    public required string Categoria { get; init; }
+    public string Metodo { get; init; } = "Cartão de Crédito";
+    public string Descricao { get; init; } = default!;
+    public required decimal Valor { get; init; }
+    public required DateOnly DataVencimento { get; init; }
+    public required bool Pago { get; init; }
+    public required bool Cancelado { get; init; }
+    public required bool Deletado { get; init; }
+    public DateOnly? DataPagamento { get; init; }
+    public string? Observacoes { get; init; }
+    public required string AlunoRegistro { get; init; }
+}
