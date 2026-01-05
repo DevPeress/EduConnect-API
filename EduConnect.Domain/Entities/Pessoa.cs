c505d7fcd087a4ffd51028123e082558318ab6ae
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-05 11:28:15 -0300

    Ajustes das entidades

--- EduConnect.Domain/Entities/Pessoa.cs
+++ EduConnect.Domain/Entities/Pessoa.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EduConnect.Domain;
+namespace EduConnect.Domain.Entities;
 
 [NotMapped]
 public abstract class Pessoa
