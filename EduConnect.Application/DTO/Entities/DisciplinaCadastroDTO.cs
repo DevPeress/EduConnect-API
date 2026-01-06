e0be386d94de5f7f010a87fd4b1804cc308b195b
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-06 13:18:34 -0300

    Criando do DTO para criar turma

---
+++ EduConnect.Application/DTO/Entities/DisciplinaCadastroDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace EduConnect.Application.DTO.Entities;
+
+public class DisciplinaCadastroDTO
+{
+    public required string Registro { get; set; }
+    public required string Nome { get; set; }
+    public required string Descricao { get; set; }
+}
