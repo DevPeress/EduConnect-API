e8470367f400c457c81947bdcf02cc2b52552007
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-15 18:02:37 -0300

    Migration para mudar dados de turma

---
+++ EduConnect.Infra.Data/Migrations/20260115202942_Turmas.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduConnect.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Turmas : MigrationWithSP
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            RunSP(migrationBuilder);
+            migrationBuilder.DropForeignKey(
+                name: "FK_Turmas_Professores_ProfessorId",
+                table: "Turmas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProfessorId",
+                table: "Turmas",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Inicio",
+                table: "Turmas",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "Fim",
+                table: "Turmas",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AnoLetivo",
+                table: "Turmas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfessorResponsavel",
+                table: "Turmas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Turmas_Professores_ProfessorId",
+                table: "Turmas",
+                column: "ProfessorId",
+                principalTable: "Professores",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Turmas_Professores_ProfessorId",
+                table: "Turmas");
+
+            migrationBuilder.DropColumn(
+                name: "ProfessorResponsavel",
+                table: "Turmas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProfessorId",
+                table: "Turmas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Inicio",
+                table: "Turmas",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Fim",
+                table: "Turmas",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "AnoLetivo",
+                table: "Turmas",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Turmas_Professores_ProfessorId",
+                table: "Turmas",
+                column: "ProfessorId",
+                principalTable: "Professores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
