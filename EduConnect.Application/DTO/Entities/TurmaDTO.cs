c1f7148aae3dc2116c0cacd9b33312f8eed47323
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-15 17:26:51 -0300

    Ajuste dos DTO de Turmas para TimeOnly

--- EduConnect.Application/DTO/Entities/TurmaDTO.cs
+++ EduConnect.Application/DTO/Entities/TurmaDTO.cs
@@ -7,8 +7,8 @@ public record TurmaDTO
     public required string Registro { get; set; }
     public required string Nome { get; set; }
     public required string Turno { get; set; }
-    public required DateTime Inicio { get; set; }
-    public required DateTime Fim { get; set; }
+    public required TimeOnly Inicio { get; set; }
+    public required TimeOnly Fim { get; set; }
     public required string Sala { get; set; }
     public required string Status { get; set; }
     public TurmaDTO() { }
--- EduConnect.Application/DTO/Entities/TurmaUpdateDTO.cs
+++ EduConnect.Application/DTO/Entities/TurmaUpdateDTO.cs
@@ -8,13 +8,13 @@ public record TurmaUpdateDTO
     public required string Nome { get; set; }
     public required string Turno { get; set; }
     public required string Status { get; set; }
-    public required DateOnly AnoEletivo { get; set; }
+    public required string AnoEletivo { get; set; }
     public required int Capacidade { get; set; }
-    public required int ProfessorResponsavel { get; set; }
-    public required DateTime Inicio { get; set; }
-    public required DateTime Fim { get; set; }
+    public required string ProfessorResponsavel { get; set; }
+    public required TimeOnly Inicio { get; set; }
+    public required TimeOnly Fim { get; set; }
     public required string Sala { get; set; }
-    public required string Dias { get; set; }
+    public required List<string> Dias { get; set; }
     public required bool Deletado { get; set; } 
     public required ICollection<TurmaDisciplina> TurmaDisciplina { get; set; }
     public required ICollection<Aluno> Alunos { get; set; }
