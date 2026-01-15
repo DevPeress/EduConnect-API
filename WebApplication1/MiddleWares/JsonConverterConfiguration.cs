32aec099ad1361c0be670c5ed043554fc050cb75
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-15 17:27:21 -0300

    Adicionando para o json converter o TimeOnly de string

--- WebApplication1/MiddleWares/JsonConverterConfiguration.cs
+++ WebApplication1/MiddleWares/JsonConverterConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace EduConnect.MiddleWares;
@@ -10,20 +11,51 @@ public static class JsonConverterConfiguration
         services.AddControllers().AddJsonOptions(options =>
         {
             options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverterConfiguration());
+            options.JsonSerializerOptions.Converters.Add(new TimeOnlyJsonConverterConfiguration());
         });
+
         return services;
     }
 
+    // =========================
+    // DateOnly
+    // =========================
     internal class DateOnlyJsonConverterConfiguration : JsonConverter<DateOnly>
     {
         private const string Format = "yyyy-MM-dd";
 
         public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return DateOnly.ParseExact(reader.GetString()!, Format);
+            return DateOnly.ParseExact(
+                reader.GetString()!,
+                Format,
+                CultureInfo.InvariantCulture
+            );
         }
 
         public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(Format));
+        }
+    }
+
+    // =========================
+    // TimeOnly
+    // =========================
+    internal class TimeOnlyJsonConverterConfiguration : JsonConverter<TimeOnly>
+    {
+        private const string Format = "HH:mm";
+
+        public override TimeOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return TimeOnly.ParseExact(
+                reader.GetString()!,
+                Format,
+                CultureInfo.InvariantCulture
+            );
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeOnly value, JsonSerializerOptions options)
         {
             writer.WriteStringValue(value.ToString(Format));
         }
