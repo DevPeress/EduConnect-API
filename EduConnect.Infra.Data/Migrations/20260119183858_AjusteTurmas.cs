b1f274dcd1b8ffdd6cfebb27cc90d63dc62229ba
Fabrício <fabricioperesdsantos@gmail.com>
2026-01-21 16:27:06 -0300

    Migration

---
+++ EduConnect.Infra.Data/Migrations/20260119183858_AjusteTurmas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduConnect.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteTurmas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dias",
+                table: "Turmas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "[]");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnoLetivo",
+                table: "TurmaDisciplinas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dias",
+                table: "Turmas");
+
+            migrationBuilder.DropColumn(
+                name: "AnoLetivo",
+                table: "TurmaDisciplinas");
+        }
+    }
+}
