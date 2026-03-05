34f72b5133e34e56fdf50750ffaab6dc801b61e3
Fabrício <fabricioperesdsantos@gmail.com>
2026-03-05 17:35:25 -0300

    Criação do ioc do mapper

---
+++ EduConnect.Infra.IoC/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EduConnect.Application.DTO.Entities;
+using EduConnect.Domain.Entities;
+
+namespace EduConnect.Infra.IoC
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Aluno, AlunoDTO>().ReverseMap();
+            CreateMap<ContaDTO, Conta>().ReverseMap();
+            CreateMap<DisciplinaCadastroDTO, Disciplinas>().ReverseMap();
+            CreateMap<FinanceiroDTO, Financeiro>().ReverseMap();
+            CreateMap<FinanceiroCadastroDTO, Financeiro>().ReverseMap();
+            CreateMap<FinanceiroUpdateDTO, Financeiro>().ReverseMap();
+            CreateMap<FuncionarioDTO, Funcionario>().ReverseMap();
+            CreateMap<FuncionarioCadastroDTO, Funcionario>().ReverseMap();
+            CreateMap<FuncionarioUpdateDTO, Funcionario>().ReverseMap();
+            CreateMap<ProfessorDTO, Professor>().ReverseMap();
+            CreateMap<ProfessorCadastroDTO, Professor>().ReverseMap();
+            CreateMap<ProfessorUpdateDTO, Professor>().ReverseMap();
+            CreateMap<RegistroDTO, Registro>().ReverseMap();
+            CreateMap<Turma, TurmaDTO>()
+            .ForMember(dest => dest.Professor,
+                opt => opt.MapFrom(src => src.ProfessorResponsavel))
+            .ForMember(dest => dest.Horario,
+                opt => opt.MapFrom(src => $"{src.Inicio} - {src.Fim}"));
+            CreateMap<TurmaDTO, Turma>();
+            CreateMap<TurmaCadastroDTO, Turma>().ReverseMap();
+            CreateMap<TurmaUpdateDTO, Turma>().ReverseMap();
+        }
+    }
+}
