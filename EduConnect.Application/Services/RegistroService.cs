5de3ec6e7479ce9de30d8bc9627c76a34d0a0bcf
Fabrício <fabricioperesdsantos@gmail.com>
2026-03-05 17:31:20 -0300

    Inserindo mapper

--- EduConnect.Application/Services/RegistroService.cs
+++ EduConnect.Application/Services/RegistroService.cs
@@ -1,12 +1,15 @@
-﻿using EduConnect.Application.DTO.Entities;
+﻿using AutoMapper;
+using EduConnect.Application.DTO.Entities;
 using EduConnect.Domain.Entities;
 using EduConnect.Domain.Interfaces;
 
 namespace EduConnect.Application.Services;
 
-public class RegistroService(IRegistroRepository repo)
+public class RegistroService(IRegistroRepository repo, IMapper mapper)
 {
     private readonly IRegistroRepository _registroRepository = repo;
+    private readonly IMapper _mapper = mapper;
+
     public async Task<(List<RegistroDTO>, int TotalRegistro)> GetRegistros(FiltroRegistroDTO filtrodto)
     {
         var filtro = new FiltroRegistro
@@ -19,19 +22,7 @@ public class RegistroService(IRegistroRepository repo)
 
         var (registros, total) = await _registroRepository.GetRegistrosAsync(filtro);
 
-        List<RegistroDTO> registrosDTO = [];
-        foreach (var registro in registros)
-        {
-            registrosDTO.Add(new RegistroDTO
-            {
-                UserName = registro.UserName,
-                UserRole = registro.UserRole,
-                Action = registro.Action.ToString(),
-                Entity = registro.Entity,
-                Description = registro.Detalhes,
-                CreatedAt = registro.CreatedAt
-            });
-        }
+        List<RegistroDTO> registrosDTO = _mapper.Map<List<RegistroDTO>>(registros);
 
         return (registrosDTO, total);
     }
