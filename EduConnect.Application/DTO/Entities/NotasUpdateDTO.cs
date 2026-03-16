1865b7033466da49f8bfb48425d7f2b43e8ac560
Fabrício Peres <fabricioperesdsantos@gmail.com>
2026-03-16 17:57:38 -0300

    Criação das Notas

---
+++ EduConnect.Application/DTO/Entities/NotasUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EduConnect.Application.DTO.Entities;
+
+public class NotasUpdateDTO
+{
+    public required int Registro { get; set; }
+    public int Nota { get; set; }
+    public required string Materia { get; set; }
+    public required string Professor { get; set; }
+}
